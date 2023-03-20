35d09123c97909d3894209f90675bad8b645badf
James Earle <j_earle@hotmail.com>
2023-03-20 11:43:11 -0700

    initial smart contract dev setup

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+      }
+    ]
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  typechain: {
+    outDir: "typechain"
+  },
+  mocha: {
+    timeout: 40000
+  }
+};
+
+export default config;
