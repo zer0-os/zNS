d2123a2dbb2051d29dc80d9774ed3766dd830801
beardhero <damienburbine@gmail.com>
2023-03-23 11:23:45 -0600

    normalization unit test

--- test/namehash.test.ts
+++ test/namehash.test.ts
@@ -13,12 +13,19 @@ describe("Namehash", function () {
     namehash = await namehashFactory.deploy();
     await namehash.deployed();
   });
-
+  
   it("should return the correct namehash for a given domain", async function () {
     const domain = "example";
     const expectedName = hre.ethers.utils.namehash(`${domain}.eth`);
 
     const name = await namehash.getNamehash(domain);
     expect(name).to.equal(expectedName);
+  });
+  it("Should normalize the name", async function () {
+    const domain = "wilder.world.citizen";
+    const domainAbnormal = "Wilder.World.Citizen";
+    const name = hre.ethers.utils.namehash(domain);
+    const abnormalName = hre.ethers.utils.namehash(domainAbnormal);
+    expect(name).to.equal(abnormalName);
   });
 });
