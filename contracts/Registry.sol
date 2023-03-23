fa5f1234442668620a11c80f4cdb2b77ea1361f6
beardhero <damienburbine@gmail.com>
2023-03-23 10:20:52 -0600

    draft of registration example

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -3,4 +3,25 @@ pragma solidity ^0.8.18;
 
 contract Registry {
   uint public number;
+  bytes32 public baseNode; // The namehash of the TLD (eg, wilder.)
+  mapping(bytes32 => address) public addressOf;
+  
+  ///@notice A placeholder registration function for an example of the namehash process
+  ///@notice This function is not complete, it doesnt perform all the necessary safety checks and logic it should eventually perform
+  ///@param namehash The namehash of the human-readable name
+  ///@param registrant The address to give ownership of the domain
+  function register(string memory namehash, address registrant) external {
+    bytes32 node = keccak256(bytes(namehash));
+    bytes32 nodeHash = keccak256(abi.encodePacked(baseNode, node));
+
+    require(addressOf[nodeHash] == address(0));
+
+    addressOf[nodeHash] = registrant;
+  }
+
+  function getAddressByNamehash(string memory namehash) public view returns(address){
+    bytes32 node = keccak256(bytes(namehash));
+    bytes32 nodeHash = keccak256(abi.encodePacked(baseNode, node));
+    return addressOf[nodeHash];
+  }
 }
--- test/registry.test.ts
+++ test/registry.test.ts
@@ -16,4 +16,12 @@ describe("Initial test setup", () => {
   it ("Gets the number", async () => {
     console.log(await registry.number());
   });
+  it ("Registers the name wilder.world.citizen, and retreives the address", async () => {
+    let name = "wilder.world.citizen";
+    let namehash = hre.ethers.utils.namehash(name);
+    await registry.register(namehash, deployer.address);
+
+    let retreived = await registry.getAddressByNamehash(namehash);
+    expect(retreived).to.equal(deployer.address);
+  });
 })
