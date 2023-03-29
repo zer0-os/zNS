2d0bc750f72e877baee3798032a99fe706f15336
Will Hyde <will.hyde@outlook.com>
2023-03-28 19:02:03 -0500

    Adding Comments

--- contracts/IZNSDomainToken.sol
+++ contracts/IZNSDomainToken.sol
@@ -2,15 +2,7 @@
 pragma solidity ^0.8.18;
 
 interface IZNSDomainToken {
-  //event ZNSRegistrychanged(address value);
-
-  // TODO
-  /** Add Documentation
-   */
-
   function register(address to, uint256 tokenId) external;
-
-  // function reclaim(uint256 tokenId) external;
 
   function revoke(uint256 tokenId) external;
 }
--- contracts/ZNSDomainToken.sol
+++ contracts/ZNSDomainToken.sol
@@ -4,15 +4,17 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {IZNSDomainToken} from "./IZNSDomainToken.sol";
 
-//TODO: Add Documentation and Access Control
+//TODO: Add Access Control
 contract ZNSDomainToken is ERC721, IZNSDomainToken {
   constructor() ERC721("ZNSDomainToken", "ZDT") {}
 
+  //Mints a token with a specified tokenId, using _safeMint, and sends it to the given address
   //TODO: Add Access Control
   function register(address to, uint256 tokenId) external {
     _safeMint(to, tokenId);
   }
 
+  //Burns the token with the specified tokenId
   //TODO: Add Access Control, replace require to also allow registry to revoke
   function revoke(uint256 tokenId) external {
     require(
