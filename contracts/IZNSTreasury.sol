0f23e90cb1eacd285d4f74d34787bedf6e4caded
Kirill <dor7an@gmail.com>
2023-04-03 15:44:35 -0700

    add functions to interface

--- contracts/IZNSTreasury.sol
+++ contracts/IZNSTreasury.sol
@@ -11,4 +11,17 @@ interface IZNSTreasury {
         address indexed depositor,
         uint256 indexed amount
     );
+
+    function stakeForDomain(
+        bytes32 domainHash,
+        string name,
+        address depositor,
+        bool useFee
+    ) external;
+
+    function getStakedAmountForDomain(bytes32 domainHash) public returns (uint256);
+
+    function setZnsRegistrar(address _znsRegistrar) external;
+
+    function getZnsRegistrar() external returns (address);
 }
