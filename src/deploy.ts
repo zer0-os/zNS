7c1d70cc1ae08bfd0004ac534c02d0cdd0888d18
Kirill <dor7an@gmail.com>
2023-04-05 16:52:22 -0700

    make first --fix run

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,18 +1,18 @@
-import * as hre from "hardhat";
-import { ZNSRegistry, ZNSRegistry__factory } from "../typechain";
+import * as hre from 'hardhat';
+import { ZNSRegistry, ZNSRegistry__factory } from '../typechain';
 
-async function main() {
+async function main () {
   const [deployer, fakeResolver] = await hre.ethers.getSigners();
 
   const factory = new ZNSRegistry__factory(deployer);
-  const registry: ZNSRegistry = await factory.deploy();
+  const registry : ZNSRegistry = await factory.deploy();
 
   await registry.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
