6c3ed708c8a79d2bd8fc6ce8cf0ee900bf01474f
beardhero <damienburbine@gmail.com>
2023-04-05 21:53:50 -0600

    get Resolver tests

--- test/ZNSAddressResolver.test.ts
+++ test/ZNSAddressResolver.test.ts
@@ -6,7 +6,9 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 /**
  * TODO the registry should have a function for checking isOwnerOrOperator, 
- * rather than the modifier also being added to AddressResolver
+ * so that the AddressResolver can implement a single call in its modifier.
+ * 
+ * Consider moving these tests to ZNSRegistry since they deploy a registry.
  */
 describe("ZNSAddressResolver", function () {
     let deployer: SignerWithAddress;
@@ -15,16 +17,13 @@ describe("ZNSAddressResolver", function () {
     let owner: SignerWithAddress;
     let addr1: SignerWithAddress;
     let operator: SignerWithAddress;
-    let mockResolver: SignerWithAddress;
     const rootDomainHash = hre.ethers.constants.HashZero;
     const wilderLabel = hre.ethers.utils.id("wilder");
-    const zeroLabel = hre.ethers.utils.id("zero");
-    const wilderSubdomainHash = hre.ethers.utils.solidityKeccak256(["bytes32", "bytes32"], [rootDomainHash, wilderLabel]);
-
+    const wilderDomainNameHash = hre.ethers.utils.solidityKeccak256(["bytes32", "bytes32"], [rootDomainHash, wilderLabel]);
 
     beforeEach(async function () {
         [owner, addr1] = await hre.ethers.getSigners();
-        [deployer, operator, mockResolver] = await hre.ethers.getSigners();
+        [deployer, operator] = await hre.ethers.getSigners();
 
         const znsAddressResolverFactory = new ZNSAddressResolver__factory(deployer);
         const znsRegistryFactory = new ZNSRegistry__factory(deployer);
@@ -32,34 +31,41 @@ describe("ZNSAddressResolver", function () {
         znsRegistry = await znsRegistryFactory.deploy();
         znsAddressResolver = await znsAddressResolverFactory.deploy(znsRegistry.address);
 
+        //Initialize registry and domain
         await znsRegistry.connect(deployer).initialize(deployer.address);
-        await znsRegistry.connect(deployer).setSubdomainRecord(rootDomainHash, wilderLabel, deployer.address, mockResolver.address);
+        await znsRegistry.connect(deployer).setSubdomainRecord(rootDomainHash, wilderLabel, deployer.address, znsAddressResolver.address);
     });
 
+    it("Should get the AddressResolver", async () => { // Copy of registry tests
+        // The domain exists
+        const existResolver = await znsRegistry.getDomainResolver(wilderDomainNameHash);
+        expect(existResolver).to.eq(znsAddressResolver.address);
+
+        // The domain does not exist
+        const someDomainHash = hre.ethers.utils.id("random-record");
+        const notExistResolver = await znsRegistry.getDomainResolver(someDomainHash);
+        expect(notExistResolver).to.eq(hre.ethers.constants.AddressZero);
+    })
     it("Should have registry address correctly set", async function () {
         expect(await znsAddressResolver.registry()).to.equal(znsRegistry.address);
     });
-
     it("Should not allow non-owner address to setAddress", async function () {
-        const domainNameHash = hre.ethers.utils.solidityKeccak256(["bytes32", "bytes32"], [rootDomainHash, wilderLabel]);
-        await expect(znsAddressResolver.connect(addr1).setAddress(domainNameHash, addr1.address)).to.be.revertedWith("ZNS: Not allowed");
+        await expect(znsAddressResolver.connect(addr1).setAddress(wilderDomainNameHash, addr1.address)).to.be.revertedWith("ZNS: Not allowed");
     });
 
     it("Should allow owner to setAddress and emit event", async function () {
-        const domainNameHash = hre.ethers.utils.solidityKeccak256(["bytes32", "bytes32"], [rootDomainHash, wilderLabel]);
-        await expect(znsAddressResolver.connect(owner).setAddress(domainNameHash, addr1.address)).to.emit(znsAddressResolver, "AddressSet").withArgs(domainNameHash, addr1.address);
+        await expect(znsAddressResolver.connect(owner).setAddress(wilderDomainNameHash, addr1.address)).to.emit(znsAddressResolver, "AddressSet").withArgs(wilderDomainNameHash, addr1.address);
     });
 
     it("Should resolve address correctly", async function () {
-        const domainNameHash = hre.ethers.utils.solidityKeccak256(["bytes32", "bytes32"], [rootDomainHash, wilderLabel]);
-        await znsAddressResolver.connect(owner).setAddress(domainNameHash, addr1.address);
+        await znsAddressResolver.connect(owner).setAddress(wilderDomainNameHash, addr1.address);
 
-        const resolvedAddress = await znsAddressResolver.getAddress(domainNameHash);
+        const resolvedAddress = await znsAddressResolver.getAddress(wilderDomainNameHash);
         expect(resolvedAddress).to.equal(addr1.address);
     });
-
     it("Should support its own calculated interface ID", async function () {
-        const supported = await znsAddressResolver.supportsInterface(znsAddressResolver.calculateSelector());
+        const selector = znsAddressResolver.calculateSelector();
+        const supported = await znsAddressResolver.supportsInterface(selector);
         expect(supported).to.be.true;
     });
 
