237abb0bfa98d4a82a473b9f45518a11e8bc6758
Kirill <dor7an@gmail.com>
2023-04-19 16:15:52 -0700

    add temp registrar role

--- contracts/access/ZNSRoles.sol
+++ contracts/access/ZNSRoles.sol
@@ -5,5 +5,6 @@ pragma solidity ^0.8.18;
 abstract contract ZNSRoles {
     bytes32 public constant GOVERNOR_ROLE = keccak256("GOVERNOR_ROLE");
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+    bytes32 public constant REGISTRAR_ROLE = keccak256("REGISTRAR_ROLE");
     // TODO AC: what other roles do we need here?
 }
