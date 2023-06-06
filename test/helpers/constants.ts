bb6196897b274d32fbb0f40592c09d54d84ae22e
Kirill <dor7an@gmail.com>
2023-06-06 16:29:49 -0700

    fix types and price config defaults

--- test/helpers/constants.ts
+++ test/helpers/constants.ts
@@ -6,15 +6,15 @@ export const ZNS_DOMAIN_TOKEN_NAME = "ZNSDomainToken";
 export const ZNS_DOMAIN_TOKEN_SYMBOL = "ZDT";
 
 export const registrationFeePercDefault = BigNumber.from("222");
+export const decimalsDefault = BigNumber.from(10).pow(BigNumber.from(18));
+export const precisionDefault = BigNumber.from(10).pow(BigNumber.from(2));
+export const precisionMultiDefault = decimalsDefault.sub(precisionDefault);
 
 export const priceConfigDefault : PriceParams = {
-  maxRootDomainPrice: ethers.utils.parseEther("1"),
-  minRootDomainPrice: ethers.utils.parseEther("0.001"),
-  maxSubdomainPrice: ethers.utils.parseEther("0.2"),
-  minSubdomainPrice: ethers.utils.parseEther("0.0002"),
-  maxRootDomainLength: BigNumber.from(100),
-  baseRootDomainLength: BigNumber.from(3),
-  maxSubdomainLength: BigNumber.from(100),
-  baseSubdomainLength: BigNumber.from(3),
+  maxPrice: ethers.utils.parseEther("1"),
+  minPrice: ethers.utils.parseEther("0.001"),
+  maxLength: BigNumber.from(100),
+  baseLength: BigNumber.from(3),
   priceMultiplier: ethers.BigNumber.from("390"),
+  precisionMultiplier: precisionMultiDefault,
 };
