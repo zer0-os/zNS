39b356609833f7beea77abc53204b049e5375b89
Kirill <dor7an@gmail.com>
2023-06-26 15:51:31 -0700

    add accessor for Transparent proxy, move all proxy accs to their own folder

---
+++ contracts/oz-proxies/TransparentUpgradeableProxyAcc.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+
+abstract contract TransparentUpgradeableProxyAcc is TransparentUpgradeableProxy {}
