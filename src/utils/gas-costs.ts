19e1ba763369a1086e67d40c1761d3fea4a5ca58
Kirill <dor7an@gmail.com>
2023-06-27 10:07:15 -0700

    add current gas costs in comments as reference

--- src/utils/gas-costs.ts
+++ src/utils/gas-costs.ts
@@ -37,3 +37,7 @@ const {
 } = calcGasCostInCurrency(gasArg, gasPriceArg, ethPriceArg);
 
 console.log(`Total cost: ${totalCost.toString()} ${costCurrency}`);
+
+// register (14 gwei, $1856) = $8.84 (ENS = $8.49)
+// reclaim (14 gwei, $1856) = $1.71
+// revoke (14 gwei, $1856) = $3.03
