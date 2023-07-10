9cf8d7ca18aacc0acd68e85440769d2161d56316
Kirill <dor7an@gmail.com>
2023-07-10 16:39:28 -0700

    add rest of the AC contracts

--- contracts/access/AccessControlled.sol
+++ contracts/access/AccessControlled.sol
@@ -9,44 +9,44 @@ import { IZNSAccessController } from "./IZNSAccessController.sol";
  * that need to be implemented to provide a deterministic connection to ZNSAccessController module.
  * @dev In order to connect an arbitrary module to ZNSAccessController and it's functionality,
  * this contract needs to be inherited by the module.
-*/
+ */
 abstract contract AccessControlled {
 
     /**
-     * @dev Emitted when the access controller contract address is set.
-    */
+     * @notice Emitted when the access controller contract address is set.
+     */
     event AccessControllerSet(address accessController);
 
     /**
-     * @dev Address of the ZNSAccessController contract.
-    */
+     * @notice Address of the ZNSAccessController contract.
+     */
     IZNSAccessController internal accessController;
 
     /**
-     * @dev Modifier to make a function callable only when caller is an admin.
+     * @notice Modifier to make a function callable only when caller is an admin.
      * Implemented here to avoid declaring this in every single contract that uses it.
-    */
+     */
     modifier onlyAdmin() {
         accessController.checkAdmin(msg.sender);
         _;
     }
 
     /**
-     * @dev Virtual function to make sure the getter is always implemented in children,
+     * @notice Virtual function to make sure the getter is always implemented in children,
      * otherwise we will not be able to read the AC address in children
      */
     function getAccessController() external view virtual returns (address);
 
     /**
-     * @dev Virtual function to make sure the setter is always implemented in children,
+     * @notice Virtual function to make sure the setter is always implemented in children,
      * otherwise we will not be able to reset the AC address in children
      */
     function setAccessController(address _accessController) external virtual;
 
     /**
-     * @dev Internal function to set the access controller address.
+     * @notice Internal function to set the access controller address.
      * @param _accessController Address of the ZNSAccessController contract.
-    */
+     */
     function _setAccessController(address _accessController) internal {
         require(_accessController != address(0), "AC: _accessController is 0x0 address");
         accessController = IZNSAccessController(_accessController);
--- contracts/access/ZNSRoles.sol
+++ contracts/access/ZNSRoles.sol
@@ -2,14 +2,29 @@
 pragma solidity ^0.8.18;
 
 
+/**
+ * @title Outlines the roles used in the ZNS system
+ * @dev Inherited ONLY by {ZNSAccessController}
+ */
 abstract contract ZNSRoles {
-    // the highest rank, assigns Admins, new roles and Role Admins
+    /**
+     * @notice The highest rank, assigns Admins, new roles and Role Admins
+     */
     bytes32 public constant GOVERNOR_ROLE = keccak256("GOVERNOR_ROLE");
-    // the main maintainer role, that gets access to all system functions
+
+    /**
+     * @notice The main maintainer role, that gets access to all system functions
+     */
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    // executor can be here to future proof, if we need a new role
-    // so we don't have to upgrade all contracts
+
+    /**
+     * @notice Executor can be here to future proof, if we need a new role
+     * so we don't have to upgrade all contracts
+     */
     bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
-    // this role is here specifically for the ZNSRegistrar contract
+
+    /**
+     * @notice This role is here specifically for the ZNSRegistrar contract
+     */
     bytes32 public constant REGISTRAR_ROLE = keccak256("REGISTRAR_ROLE");
 }
