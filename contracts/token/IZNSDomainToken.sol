96a886df1345acbd9a3fd44420a38157fddb4acb
Kirill <dor7an@gmail.com>
2023-07-11 14:21:39 -0700

    add DomainToken

--- contracts/token/IZNSDomainToken.sol
+++ contracts/token/IZNSDomainToken.sol
@@ -5,27 +5,10 @@ import { IERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ER
 
 
 interface IZNSDomainToken is IERC721Upgradeable {
-    event SetAccessAuthorization(address indexed account);
-    
-    /**
-     * @notice Initialize the ERC721 ZNSDomainToken contract with the given values
-     * @param accessController The access controller contract address
-     * @param tokenName The ZNS token name
-     * @param tokenSymbol The ZNS token symbol
-     */
     function initialize(address accessController, string calldata tokenName, string calldata tokenSymbol) external;
 
-    /**
-     * @notice Mints a token with a specified tokenId, using _safeMint, and sends it to the given address
-     * @param to The address that will recieve the newly minted domain token
-     * @param tokenId The TokenId that the caller wishes to mint/register
-     */
     function register(address to, uint256 tokenId) external;
 
-    /**
-     * @notice Burns the token with the specified tokenId
-     * @param tokenId The tokenId that the caller wishes to burn/revoke
-     */
     function revoke(uint256 tokenId) external;
 
     function setAccessController(address accessController) external;
--- contracts/token/ZNSDomainToken.sol
+++ contracts/token/ZNSDomainToken.sol
@@ -8,15 +8,29 @@ import { AccessControlled } from "../access/AccessControlled.sol";
 
 
 /**
- * @title A contract for tokenizing domains under ZNS
+ * @title A contract for tokenizing domains under ZNS. Every domain in ZNS has a corresponding token
+ * minted at Register time. This token is also an NFT that is fully ERC-721 compliant.
+ * @dev Note that all ZNS related functions on this contract can ONLY be called by either
+ * the {ZNSRegistrar} contract or any address holding a REGISTRAR_ROLE.
  */
 contract ZNSDomainToken is AccessControlled, UUPSUpgradeable, ERC721Upgradeable, IZNSDomainToken {
 
+    /**
+     * @notice Modifier used in functions to be called only by the {ZNSRegistrar} contract
+     * or address with REGISTRAR_ROLE.
+     */
     modifier onlyRegistrar {
         accessController.checkRegistrar(msg.sender);
         _;
     }
 
+    /**
+     * @notice Initializer for the {ZNSDomainToken} proxy.
+     * Note that this function does NOT have role protection enforced!
+     * @param accessController_ The address of the {ZNSAccessController} contract
+     * @param name_ The name of the token
+     * @param symbol_ The symbol of the token
+     */
     function initialize(
         address accessController_,
         string memory name_,
@@ -27,22 +41,30 @@ contract ZNSDomainToken is AccessControlled, UUPSUpgradeable, ERC721Upgradeable,
     }
 
     /**
-     * @notice Mints a token with a specified tokenId, using _safeMint, and sends it to the given address
-     * @param to The address that will recieve the newly minted domain token
-     * @param tokenId The TokenId that the caller wishes to mint/register
+     * @notice Mints a token with a specified tokenId, using _safeMint, and sends it to the given address.
+     * Used ONLY as a part of the Register flow that starts from {`ZNSRegistrar.registerDomain()`}!
+     * @param to The address that will recieve the newly minted domain token (new domain owner)
+     * @param tokenId The TokenId that the caller wishes to mint/register. TokenId is created
+     * as a hash of the domain name casted to uint256.
      */
     function register(address to, uint256 tokenId) external override onlyRegistrar {
         _safeMint(to, tokenId);
     }
 
     /**
-     * @notice Burns the token with the specified tokenId
-     * @param tokenId The tokenId that the caller wishes to burn/revoke
+     * @notice Burns the token with the specified tokenId.
+     * Used ONLY as a part of the Revoke flow that starts from {`ZNSRegistrar.revokeDomain()`}!
+     * @param tokenId The tokenId (as `uint256(domainHash)`) that the caller wishes to burn/revoke
      */
     function revoke(uint256 tokenId) external override onlyRegistrar {
         _burn(tokenId);
     }
 
+    /**
+     * @dev Sets the address of the {ZNSAccessController} contract.
+     * Can only be called by the ADMIN. Emits an {AccessControllerSet} event.
+     * @param accessController The address of the {ZNSAccessController} contract
+     */
     function setAccessController(address accessController_)
     external
     override(AccessControlled, IZNSDomainToken)
@@ -51,6 +73,9 @@ contract ZNSDomainToken is AccessControlled, UUPSUpgradeable, ERC721Upgradeable,
         _setAccessController(accessController_);
     }
 
+    /**
+     * @dev Returns the address of the {ZNSAccessController} contract saved in state.
+     */
     function getAccessController() external view override(AccessControlled, IZNSDomainToken) returns (address) {
         return address(accessController);
     }
