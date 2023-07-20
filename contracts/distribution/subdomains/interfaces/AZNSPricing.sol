d4634780cacafafd455339262501d1eed141b802
Kirill <dor7an@gmail.com>
2023-07-20 11:38:13 -0700

    fix zns pricing abstract

--- contracts/distribution/subdomains/interfaces/AZNSPricing.sol
+++ contracts/distribution/subdomains/interfaces/AZNSPricing.sol
@@ -6,5 +6,8 @@ pragma solidity ^0.8.18;
 abstract contract AZNSPricing {
     // TODO sub: do we leave only return of the price
     //  or do we leave fee values here as well?
-    function getPrice(string calldata name) external virtual view returns (uint256);
+    function getPrice(
+        bytes32 parentHash,
+        string calldata name
+    ) external virtual view returns (uint256);
 }
