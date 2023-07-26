8415f3e2857ca6c6c0b13ef854a448f26bc2e511
Kirill <dor7an@gmail.com>
2023-07-26 15:29:48 -0700

    add access control, add setters for the distr configs, update interface

--- contracts/distribution/subdomains/IZNSSubdomainRegistrar.sol
+++ contracts/distribution/subdomains/IZNSSubdomainRegistrar.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.18;
 
 import { IDistributionConfig } from "./IDistributionConfig.sol";
+import { AZNSPricing } from "./abstractions/AZNSPricing.sol";
+import { AZNSPayment } from "./abstractions/AZNSPayment.sol";
 
 
 interface IZNSSubdomainRegistrar is IDistributionConfig {
@@ -17,8 +19,33 @@ interface IZNSSubdomainRegistrar is IDistributionConfig {
         string calldata name
     ) external pure returns (bytes32);
 
-    function setParentRules(
+    function setDistributionConfigForDomain(
         bytes32 parentHash,
         DistributionConfig calldata config
     ) external;
+
+    function setPricingContractForDomain(
+        bytes32 domainHash,
+        AZNSPricing pricingContract
+    ) external;
+
+    function setPaymentContractForDomain(
+        bytes32 domainHash,
+        AZNSPayment paymentContract
+    ) external;
+
+    function setAccessTypeForDomain(
+        bytes32 domainHash,
+        AccessType accessType
+    ) external;
+
+    function setWhitelistForDomain(
+        bytes32 domainHash,
+        address registrant,
+        bool allowed
+    ) external;
+
+    function getAccessController() external view returns (address);
+
+    function setAccessController(address accessController_) external;
 }
--- contracts/distribution/subdomains/ZNSSubdomainRegistrar.sol
+++ contracts/distribution/subdomains/ZNSSubdomainRegistrar.sol
@@ -6,23 +6,39 @@ import { AZNSPayment } from "./abstractions/AZNSPayment.sol";
 import { IZNSRegistry } from "../../registry/IZNSRegistry.sol";
 import { IZNSRegistrar } from "../IZNSRegistrar.sol";
 import { IZNSSubdomainRegistrar } from "./IZNSSubdomainRegistrar.sol";
+import "../../access/AccessControlled.sol";
 
 
-contract ZNSSubdomainRegistrar is IZNSSubdomainRegistrar {
+contract ZNSSubdomainRegistrar is AccessControlled, IZNSSubdomainRegistrar {
 
     IZNSRegistry public registry;
     // TODO sub: change name of Registrar var and the contract also
     IZNSRegistrar public mainRegistrar;
 
     // TODO sub: make better name AND for the setter function !
-    mapping(bytes32 domainHash => DistributionConfig) public parentRules;
+    mapping(bytes32 domainHash => DistributionConfig) public distrConfigs;
 
     mapping(bytes32 domainHash =>
         mapping(address registrant => bool allowed)
     ) public distributionWhitelist;
 
+    modifier onlyOwnerOperatorOrRegistrar(bytes32 domainHash) {
+        require(
+            registry.isOwnerOrOperator(domainHash, msg.sender)
+            || accessController.isRegistrar(msg.sender),
+            "ZNSSubdomainRegistrar: Not authorized"
+        );
+        _;
+    }
+
     // TODO sub: proxy ??
-    constructor(address _registry, address _registrar) {
+    constructor(
+        address _accessController,
+        address _registry,
+        address _registrar
+    ) {
+        _setAccessController(_accessController);
+        // TODO sub: switch below to functions ??
         require(
             _registry != address(0),
             "ZNSSubdomainRegistrar: _registry can not be 0x0 address"
@@ -43,7 +59,7 @@ contract ZNSSubdomainRegistrar is IZNSSubdomainRegistrar {
         DistributionConfig calldata configForSubdomains
     ) external override {
         // TODO sub: make the order of ops better
-        DistributionConfig memory parentConfig = parentRules[parentHash];
+        DistributionConfig memory parentConfig = distrConfigs[parentHash];
         require(
             parentConfig.accessType != AccessType.LOCKED
                 || registry.isOwnerOrOperator(parentHash, msg.sender),
@@ -73,10 +89,6 @@ contract ZNSSubdomainRegistrar is IZNSSubdomainRegistrar {
             price
         );
 
-        // TODO sub: what is the best way to do this ??
-        //      so that it can be done for root domain also
-        setParentRules(subdomainHash, configForSubdomains);
-
         mainRegistrar.settleRegistration(
             parentHash,
             subdomainHash,
@@ -84,6 +96,10 @@ contract ZNSSubdomainRegistrar is IZNSSubdomainRegistrar {
             msg.sender,
             domainAddress
         );
+
+        // TODO sub: what is the best way to do this ??
+        //      so that it can be done for root domain also
+        setDistributionConfigForDomain(subdomainHash, configForSubdomains);
     }
 
     function hashWithParent(
@@ -99,12 +115,67 @@ contract ZNSSubdomainRegistrar is IZNSSubdomainRegistrar {
     }
 
     // TODO sub: access control
-    function setParentRules(
-        bytes32 parentHash,
+    function setDistributionConfigForDomain(
+        bytes32 domainHash,
         DistributionConfig calldata config
     ) public override {
-        // TODO sub: expand!
-        parentRules[parentHash] = config;
+        setPricingContractForDomain(domainHash, config.pricingContract);
+        setPaymentContractForDomain(domainHash, config.paymentContract);
+        setAccessTypeForDomain(domainHash, config.accessType);
+    }
+
+    function setPricingContractForDomain(
+        bytes32 domainHash,
+        AZNSPricing pricingContract
+    ) public override onlyOwnerOperatorOrRegistrar(domainHash) {
+        require(
+            address(pricingContract) != address(0),
+            "ZNSSubdomainRegistrar: pricingContract can not be 0x0 address"
+        );
+
+        distrConfigs[domainHash].pricingContract = pricingContract;
         // TODO sub: emit event
+    }
+
+    function setPaymentContractForDomain(
+        bytes32 domainHash,
+        AZNSPayment paymentContract
+    ) public override onlyOwnerOperatorOrRegistrar(domainHash) {
+        require(
+            address(paymentContract) != address(0),
+            "ZNSSubdomainRegistrar: paymentContract can not be 0x0 address"
+        );
+
+        distrConfigs[domainHash].paymentContract = AZNSPayment(paymentContract);
+        // TODO sub: emit event
+    }
+
+    function setAccessTypeForDomain(
+        bytes32 domainHash,
+        AccessType accessType
+    ) public override onlyOwnerOperatorOrRegistrar(domainHash) {
+        distrConfigs[domainHash].accessType = accessType;
+        // TODO sub: emit event
+    }
+
+    // TODO sub: iron this out !!
+    function setWhitelistForDomain(
+        bytes32 domainHash,
+        address registrant,
+        bool allowed
+    ) external override onlyOwnerOperatorOrRegistrar(domainHash) {
+        distributionWhitelist[domainHash][registrant] = allowed;
+        // TODO sub: emit event ??
+    }
+
+    function getAccessController() external view override(AccessControlled, IZNSSubdomainRegistrar) returns (address) {
+        return address(accessController);
+    }
+
+    function setAccessController(address accessController_)
+    external
+    override(AccessControlled, IZNSSubdomainRegistrar)
+    onlyAdmin {
+        _setAccessController(accessController_);
     }
 }
