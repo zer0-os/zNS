d80380f33bc18eb931319966d0c2dcfa62a4cdc1
Kirill <dor7an@gmail.com>
2023-07-31 14:34:55 -0700

    add asymptotic pricing contract

---
+++ contracts/distribution/subdomains/pricing/ZNSAsymptoticPricing.sol
@@ -0,0 +1,177 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { AZNSPricingWithFee } from "../abstractions/AZNSPricingWithFee.sol";
+import { StringUtils } from "../../../utils/StringUtils.sol";
+// TODO sub: possibly remove if moved to an interface
+import { IDomainPriceConfig } from "../abstractions/IDomainPriceConfig.sol";
+
+
+// TODO sub: figure out how to interface here with the abstract and PriceConfig struct !!
+contract ZNSAsymptoticPricing is AZNSPricingWithFee, IDomainPriceConfig {
+    using StringUtils for string;
+
+    // TODO sub: possibly move to an interface
+    event MaxPriceSet(bytes32 indexed domainHash, uint256 price);
+    event MinPriceSet(bytes32 indexed domainHash, uint256 price);
+    event BaseLengthSet(bytes32 indexed domainHash, uint256 length);
+    event MaxLengthSet(bytes32 indexed domainHash, uint256 length);
+    event PrecisionMultiplierSet(bytes32 indexed domainHash, uint256 precision);
+    event FeePercentageSet(bytes32 indexed domainHash, uint256 feePercentage);
+    event PriceConfigSet(
+        bytes32 indexed domainHash,
+        uint256 maxPrice,
+        uint256 minPrice,
+        uint256 maxLength,
+        uint256 baseLength,
+        uint256 precisionMultiplier,
+        uint256 feePercentage
+    );
+
+    uint256 public constant PERCENTAGE_BASIS = 10000;
+
+    mapping(bytes32 => DomainPriceConfig) public priceConfigs;
+
+    function getPrice(
+        bytes32 parentHash,
+        string calldata label
+    ) public view override returns (uint256) {
+        uint256 length = label.strlen();
+        // No pricing is set for 0 length domains
+        if (length == 0) return 0;
+
+        return _getPrice(parentHash, label.strlen());
+    }
+
+    function getPriceAndFee(
+        bytes32 parentHash,
+        string calldata label
+    ) external view override returns (uint256 price, uint256 fee) {
+        price = getPrice(parentHash, label);
+        fee = getFeeForPrice(parentHash, price);
+        return (price, fee);
+    }
+
+    function getFeeForPrice(
+        bytes32 parentHash,
+        uint256 price
+    ) public view returns (uint256) {
+        return price * priceConfigs[parentHash].feePercentage / PERCENTAGE_BASIS;
+    }
+
+    // TODO sub: add AC !!!
+    function setPriceConfig(bytes32 domainHash, DomainPriceConfig calldata priceConfig) external {
+        priceConfigs[domainHash].baseLength = priceConfig.baseLength;
+        priceConfigs[domainHash].maxPrice = priceConfig.maxPrice;
+        priceConfigs[domainHash].minPrice = priceConfig.minPrice;
+        priceConfigs[domainHash].maxLength = priceConfig.maxLength;
+        priceConfigs[domainHash].feePercentage = priceConfig.feePercentage;
+        setPrecisionMultiplier(domainHash, priceConfig.precisionMultiplier);
+
+        _validateConfig(domainHash);
+
+        emit PriceConfigSet(
+            domainHash,
+            priceConfig.maxPrice,
+            priceConfig.minPrice,
+            priceConfig.maxLength,
+            priceConfig.baseLength,
+            priceConfig.precisionMultiplier,
+            priceConfig.feePercentage
+        );
+    }
+
+    function setMaxPrice(
+        bytes32 domainHash,
+        uint256 maxPrice
+    ) external {
+        priceConfigs[domainHash].maxPrice = maxPrice;
+
+        if (maxPrice != 0) _validateConfig(domainHash);
+
+        emit MaxPriceSet(domainHash, maxPrice);
+    }
+
+    function setMinPrice(
+        bytes32 domainHash,
+        uint256 minPrice
+    ) external {
+        priceConfigs[domainHash].minPrice = minPrice;
+
+        _validateConfig(domainHash);
+
+        emit MinPriceSet(domainHash, minPrice);
+    }
+
+    function setBaseLength(
+        bytes32 domainHash,
+        uint256 length
+    ) external {
+        priceConfigs[domainHash].baseLength = length;
+
+        _validateConfig(domainHash);
+
+        emit BaseLengthSet(domainHash, length);
+    }
+
+    function setMaxLength(
+        bytes32 domainHash,
+        uint256 length
+    ) external {
+        priceConfigs[domainHash].maxLength = length;
+
+        if (length != 0) _validateConfig(domainHash);
+
+        emit MaxLengthSet(domainHash, length);
+    }
+
+    function setPrecisionMultiplier(
+        bytes32 domainHash,
+        uint256 multiplier
+    ) public {
+        require(multiplier != 0, "ZNSAsymptoticPricing: precisionMultiplier cannot be 0");
+        require(multiplier <= 10**18, "ZNSAsymptoticPricing: precisionMultiplier cannot be greater than 10^18");
+        priceConfigs[domainHash].precisionMultiplier = multiplier;
+
+        emit PrecisionMultiplierSet(domainHash, multiplier);
+    }
+
+    function setFeePercentage(bytes32 domainHash, uint256 feePercentage)
+    external {
+        priceConfigs[domainHash].feePercentage = feePercentage;
+        emit FeePercentageSet(domainHash, feePercentage);
+    }
+
+    function _getPrice(
+        bytes32 parentHash,
+        uint256 labelLength
+    ) internal view returns (uint256) {
+        DomainPriceConfig memory config = priceConfigs[parentHash];
+
+        // Setting baseLength to 0 indicates to the system that we are
+        // currently in a special phase where we define an exact price for all domains
+        // e.g. promotions or sales
+        if (config.baseLength == 0) return config.maxPrice;
+        if (labelLength <= config.baseLength) return config.maxPrice;
+        if (labelLength > config.maxLength) return config.minPrice;
+
+        return
+            (config.baseLength * config.maxPrice / labelLength)
+            / config.precisionMultiplier * config.precisionMultiplier;
+    }
+
+    /**
+     * @notice Internal function called every time we set props of `priceConfigs[domainHash]`
+     * to make sure that values being set can not disrupt the price curve or zero out prices
+     * for domains. If this validation fails, function will revert.
+     * @dev We are checking here for possible price spike at `maxLength`
+     * which can occur if some of the config values are not properly chosen and set.
+     */
+    function _validateConfig(bytes32 domainHash) internal view {
+        uint256 prevToMinPrice = _getPrice(domainHash, priceConfigs[domainHash].maxLength - 1);
+        require(
+            priceConfigs[domainHash].minPrice < prevToMinPrice,
+            "ZNSAsymptoticPricing: incorrect value set causes the price spike at maxLength."
+        );
+    }
+}
