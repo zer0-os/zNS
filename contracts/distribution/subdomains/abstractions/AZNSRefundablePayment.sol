dabd4844733a71d871f5286774995903694b4713
Kirill <dor7an@gmail.com>
2023-08-02 14:45:54 -0700

    add refundProcesses event to AZNSRefundablePayment

--- contracts/distribution/subdomains/abstractions/AZNSRefundablePayment.sol
+++ contracts/distribution/subdomains/abstractions/AZNSRefundablePayment.sol
@@ -5,6 +5,13 @@ import { AZNSPayment } from "./AZNSPayment.sol";
 
 
 abstract contract AZNSRefundablePayment is AZNSPayment {
+    event RefundProcessed(
+        bytes32 indexed parentHash,
+        bytes32 indexed domainHash,
+        address indexed domainOwner,
+        uint256 amount
+    );
+
     function refundsOnRevoke() external pure override returns (bool) {
         return true;
     }
