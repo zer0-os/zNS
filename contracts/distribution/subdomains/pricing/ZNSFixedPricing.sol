719f0053ffac071a0d5ff4ad08606ede406c282b
Kirill <dor7an@gmail.com>
2023-08-16 16:42:46 -0700

    rework FixedPricing to support fees

--- contracts/distribution/subdomains/pricing/ZNSFixedPricing.sol
+++ contracts/distribution/subdomains/pricing/ZNSFixedPricing.sol
@@ -1,40 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import { AZNSPricing } from "../abstractions/AZNSPricing.sol";
 import { IZNSRegistry } from "../../../registry/IZNSRegistry.sol";
 import { AAccessControlled } from "../../../access/AAccessControlled.sol";
 import { ARegistryWired } from "../../../abstractions/ARegistryWired.sol";
+import { AZNSPricingWithFee } from "../abstractions/AZNSPricingWithFee.sol";
 
 
-contract ZNSFixedPricing is AAccessControlled, ARegistryWired, AZNSPricing {
+contract ZNSFixedPricing is AAccessControlled, ARegistryWired, AZNSPricingWithFee {
 
-    event PriceChanged(bytes32 indexed parentHash, uint256 newPrice);
+    event PriceSet(bytes32 indexed parentHash, uint256 indexed newPrice);
+    event FeePercentageSet(bytes32 indexed parentHash, uint256 indexed feePercentage);
 
-    // TODO sub: do we need to add fees here ??
-    //  what is this is used with the stake payment ??
-    mapping(bytes32 domainHash => uint256 price) internal prices;
+    struct PriceConfig {
+        uint256 price;
+        uint256 feePercentage;
+    }
+
+    uint256 public constant PERCENTAGE_BASIS = 10000;
+
+
+    mapping(bytes32 domainHash => PriceConfig config) internal priceConfigs;
 
     constructor(address _accessController, address _registry) {
         _setAccessController(_accessController);
         setRegistry(_registry);
     }
 
-    function setPrice(bytes32 domainHash, uint256 _price) external onlyOwnerOrOperator(domainHash) {
-        prices[domainHash] = _price;
+    function setPrice(bytes32 domainHash, uint256 _price) public onlyOwnerOrOperator(domainHash) {
+        priceConfigs[domainHash].price = _price;
 
-        emit PriceChanged(domainHash, _price);
+        emit PriceSet(domainHash, _price);
     }
 
-    function getPrice(bytes32 parentHash, string calldata label) external override view returns (uint256) {
-        return prices[parentHash];
+    function getPrice(bytes32 parentHash, string calldata label) public override view returns (uint256) {
+        return priceConfigs[parentHash].price;
     }
 
     // TODO sub: is this a viable solution to not pay for subdomains
     //  of a revoked parent ?? this lets us wipe the price at any time for the parent
     function revokePrice(bytes32 domainHash) external override onlyRegistrar {
-        prices[domainHash] = 0;
+        priceConfigs[domainHash].price = 0;
+        priceConfigs[domainHash].feePercentage = 0;
         emit PriceRevoked(domainHash);
+    }
+
+    function setFeePercentage(
+        bytes32 domainHash,
+        uint256 feePercentage
+    ) public onlyOwnerOrOperator(domainHash) {
+        priceConfigs[domainHash].feePercentage = feePercentage;
+        emit FeePercentageSet(domainHash, feePercentage);
+    }
+
+    // TODO sub: do we need both of these functions ??
+    function getFeeForPrice(
+        bytes32 parentHash,
+        uint256 price
+    ) public view override returns (uint256) {
+        return (price * priceConfigs[parentHash].feePercentage) / PERCENTAGE_BASIS;
+    }
+
+    function getPriceAndFee(
+        bytes32 parentHash,
+        string calldata label
+    ) external view override returns (uint256 price, uint256 fee) {
+        price = getPrice(parentHash, label);
+        fee = getFeeForPrice(parentHash, price);
+        return (price, fee);
+    }
+
+    function setPriceConfig(
+        bytes32 domainHash,
+        PriceConfig calldata priceConfig
+    ) external {
+        setPrice(domainHash, priceConfig.price);
+        setFeePercentage(domainHash, priceConfig.feePercentage);
     }
 
     function setRegistry(address registry_) public override onlyAdmin {
