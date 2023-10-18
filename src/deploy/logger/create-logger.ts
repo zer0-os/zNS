cfa3dd9c0330054bf2fda9c7b670f45c7a4ac1c9
Kirill <dor7an@gmail.com>
2023-10-17 17:09:30 -0700

    rework logger functions to save to a module var so that we use the same logger everywhere

--- src/deploy/logger/create-logger.ts
+++ src/deploy/logger/create-logger.ts
@@ -1,8 +1,11 @@
 import winston from "winston";
+import { TLogger } from "../campaign/types";
+
+let logger : TLogger | null = null;
 
 
 // TODO dep: refine this function and configurability of this logger
-export const createLogger = (logLevel ?: string) => winston.createLogger({
+export const createLogger = (logLevel ?: string, silent ?: boolean) => winston.createLogger({
   level: logLevel,
   format: winston.format.combine(
     // TODO dep: adjust the format to what we need
@@ -16,4 +19,17 @@ export const createLogger = (logLevel ?: string) => winston.createLogger({
   ],
   // TODO dep: make sure we need this to be set!
   exitOnError: false,
+  silent,
 });
+
+// TODO dep: add more ENV vars here so we don't have to pass anything
+export const getLogger = () : TLogger => {
+  if (logger) return logger;
+
+  logger = createLogger(
+    process.env.LOG_LEVEL || "debug",
+    process.env.SILENT_LOGGER === "true"
+  );
+
+  return logger;
+};
