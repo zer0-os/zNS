d91cafe3e6fd2018a0f86c26ab45b1d34feb4560
James Earle <james@zero.tech>
2023-10-26 11:42:24 -0400

    pin pragma numbers for ZNS deployment

--- contracts/registrar/ZNSSubRegistrar.sol
+++ contracts/registrar/ZNSSubRegistrar.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { IZNSPricer } from "../types/IZNSPricer.sol";
 import { IZNSRootRegistrar, CoreRegisterArgs } from "./IZNSRootRegistrar.sol";
