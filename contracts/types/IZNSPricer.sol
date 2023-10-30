7b3d418e41d46a20ace3df99111ed078b95e8cff
Kirill <dor7an@gmail.com>
2023-10-30 16:03:06 -0700

    add comment to IZNSPricer

--- contracts/types/IZNSPricer.sol
+++ contracts/types/IZNSPricer.sol
@@ -10,6 +10,13 @@ interface IZNSPricer {
     /**
      * @dev `parentHash` param is here to allow pricer contracts
      *  to have different price configs for different subdomains
+     * `skipValidityCheck` param is added to provide proper revert when the user is
+     * calling this to find out the price of a domain that is not valid. But in Registrar contracts
+     * we want to do this explicitly and before we get the price to have lower tx cost for reverted tx.
+     * So Registrars will pass this bool as "true" to not repeat the validity check.
+     * Note that if calling this function directly to find out the price, a user should always pass "false"
+     * as `skipValidityCheck` param, otherwise, the price will be returned for an invalid label that is not
+     * possible to register.
      */
     function getPrice(
         bytes32 parentHash,
