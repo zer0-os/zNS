2fce3b69311b27a27e8d25c8a0537d1cfd9a77dc
Kirill <dor7an@gmail.com>
2023-11-10 13:32:26 -0800

    remove all redundant code in TreasuryDM

--- src/deploy/missions/contracts/treasury.ts
+++ src/deploy/missions/contracts/treasury.ts
@@ -11,35 +11,8 @@ export class ZNSTreasuryDM extends BaseDeployMission {
     kind: ProxyKinds.uups,
   };
 
-  // bool for determining token setup behaviour
-  // determined in constructor
-  isMockedMeowToken : boolean;
   contractName = znsNames.treasury.contract;
   instanceName = znsNames.treasury.instance;
-
-  constructor (args : IDeployMissionArgs) {
-    super(args);
-
-    const {
-      config: {
-        stakingTokenAddress,
-      },
-    } = this.campaign;
-
-    if (!!stakingTokenAddress) {
-      this.isMockedMeowToken = false;
-    } else {
-      // TODO dep: is this a correct check? rework this whole flow in the class when MeowTokenDM is done
-      if (!this.campaign.state.missions.includes(MeowTokenDM)) throw new Error(
-        `No staking token found!
-        Please make sure to provide 'stakingTokenAddress' to the config
-        or add mocked token to the Deploy Campaign if this is a test.`
-      );
-
-      // TODO dep: possibly make an ENV var out of this so that it is known before we get here
-      this.isMockedMeowToken = true;
-    }
-  }
 
   deployArgs () : TDeployArgs {
     const {
@@ -52,23 +25,25 @@ export class ZNSTreasuryDM extends BaseDeployMission {
       },
     } = this.campaign;
 
-    const stakingToken = !this.isMockedMeowToken
-      ? stakingTokenAddress
-      : meowToken.address;
+    // TODO dep: is this needed at all? can this happen?
+    if (meowToken.address !== stakingTokenAddress) {
+      throw new Error(
+        `MEOW token address ${meowToken.address} does not match ENV set 'stakingTokenAddress' ${stakingTokenAddress}`
+      );
+    }
 
     return [
       accessController.address,
       registry.address,
-      stakingToken,
+      meowToken.address,
       zeroVaultAddress,
     ];
   }
 
   async needsPostDeploy () : Promise<boolean> {
-    return this.isMockedMeowToken;
+    return this.config.mockMeowToken;
   }
 
-  // TODO dep: figure out if this is needed to be here so it doesn't run by mistake in prod.
   // this should launch ONLY if the Meow Token was mocked in test !
   async postDeploy () {
     const {
