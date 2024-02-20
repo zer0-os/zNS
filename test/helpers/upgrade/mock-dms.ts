09360c833f774075544480f85d010c82d1287bdf
Kirill <dor7an@gmail.com>
2024-02-20 11:53:41 -0800

    make smoke test to run with zDC to find what's missing

---
+++ test/helpers/upgrade/mock-dms.ts
@@ -0,0 +1,9 @@
+/* eslint-disable max-classes-per-file */
+import { ZNSDomainTokenDM, ZNSRegistryDM } from "../../../src/deploy/missions/contracts";
+
+
+export class ZNSRegistryUpgradeMockDM extends ZNSRegistryDM {}
+
+export class ZNSRootRegistrarUpgradeMockDM extends ZNSRegistryDM {}
+
+export class ZNSDomainTokenUpgradeMockDM extends ZNSDomainTokenDM {}
