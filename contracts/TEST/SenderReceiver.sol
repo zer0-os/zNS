d9d2f79a8965e176d1db66c5a1c4534bdcc5afe3
Whytecrowe <dor7an@gmail.com>
2024-06-12 15:55:52 -0700

    add direct payload call, new state vars and more complex tests to set those in state

--- contracts/TEST/SenderReceiver.sol
+++ contracts/TEST/SenderReceiver.sol
@@ -6,43 +6,79 @@ import { IAxelarGasService } from "@axelar-network/axelar-gmp-sdk-solidity/contr
 
 
 contract SenderReceiver is AxelarExecutable {
+    error CallFailed(bytes callReturnValue);
+
+    struct Data {
+        address[] addresses;
+        uint256 num;
+        string text;
+        bytes32 hash;
+        bytes data;
+    }
+
     IAxelarGasService public immutable gasService;
     string public message;
     uint256 public status;
+    mapping(string => Data) public dataMapping;
 
     constructor(address _gateway, address _gasService) AxelarExecutable(_gateway) {
         gasService = IAxelarGasService(_gasService);
     }
 
-    function sendMessage(
-        string calldata destinationChain,
-        string calldata destinationAddress,
-        string calldata message_
-    ) external payable {
-        bytes memory payload = abi.encode(1, message_);
-
-        gasService.payNativeGasForContractCall{ value: msg.value } (
-            address(this),
-            destinationChain,
-            destinationAddress,
-            payload,
-            msg.sender
-        );
-
-        gateway.callContract(
-            destinationChain,
-            destinationAddress,
-            payload
-        );
+    function getMappingAddresses(string calldata key) external view returns (address[] memory) {
+        return dataMapping[key].addresses;
     }
 
-    function sendStatus(
+//    function sendMessage(
+//        string calldata destinationChain,
+//        string calldata destinationAddress,
+//        string calldata message_
+//    ) external payable {
+//        bytes memory payload = abi.encode(1, message_);
+//
+//        gasService.payNativeGasForContractCall{ value: msg.value } (
+//            address(this),
+//            destinationChain,
+//            destinationAddress,
+//            payload,
+//            msg.sender
+//        );
+//
+//        gateway.callContract(
+//            destinationChain,
+//            destinationAddress,
+//            payload
+//        );
+//    }
+// TODO: these won't work for now since our _execute() function sends payload directly
+//    and the code for directly calling these is removed from _execute()
+//    function sendStatus(
+//        string calldata destinationChain,
+//        string calldata destinationAddress,
+//        uint256 status_
+//    ) external payable {
+//        bytes memory payload = abi.encode(2, status_);
+//
+//        gasService.payNativeGasForContractCall{ value: msg.value } (
+//            address(this),
+//            destinationChain,
+//            destinationAddress,
+//            payload,
+//            msg.sender
+//        );
+//
+//        gateway.callContract(
+//            destinationChain,
+//            destinationAddress,
+//            payload
+//        );
+//    }
+
+    function sendPayload(
         string calldata destinationChain,
         string calldata destinationAddress,
-        uint256 status_
+        bytes calldata payload
     ) external payable {
-        bytes memory payload = abi.encode(2, status_);
-
         gasService.payNativeGasForContractCall{ value: msg.value } (
             address(this),
             destinationChain,
@@ -66,18 +102,31 @@ contract SenderReceiver is AxelarExecutable {
         status = status_;
     }
 
+    function setDataMapping(
+        string calldata key,
+        Data calldata _data
+    ) external {
+        dataMapping[key] = _data;
+    }
+
     function _execute(
         string calldata sourceChain,
         string calldata sourceAddress,
         bytes calldata payload
     ) internal override {
-        uint256 method;
-        (method) = abi.decode(payload, (uint256));
+        (bool success, bytes memory returnVal) = address(this).call(payload);
 
-        if (method == 1) {
-            (, message) = abi.decode(payload, (uint256, string));
-        } else if (method == 2) {
-            (, status) = abi.decode(payload, (uint256, uint256));
+        if (!success) {
+            revert CallFailed(returnVal);
         }
+
+// TODO: uncomment this example if you want to call sendMessage() and sendStatus() functions
+//        uint256 method;
+//        (method) = abi.decode(payload, (uint256));
+//        if (method == 1) {
+//            (, message) = abi.decode(payload, (uint256, string));
+//        } else if (method == 2) {
+//            (, status) = abi.decode(payload, (uint256, uint256));
+//        }
     }
 }
--- test/AXELAR/smoke.test.ts
+++ test/AXELAR/smoke.test.ts
@@ -4,47 +4,54 @@ import { expect } from "chai";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 import SenderReceiver from "../../artifacts/contracts/TEST/SenderReceiver.sol/SenderReceiver.json";
+// import { ethers } from "ethers";
+
+const iface = new utils.Interface(SenderReceiver.abi);
+// const iface = new ethers.Interface(SenderReceiver.abi);
 
 
-describe("Axelar Tests", () => {
+describe.only("Axelar Tests", () => {
   let ethereum : Network;
   let polygon : Network;
 
   let userEth : Wallet;
   let userPoly : Wallet;
+  let ethAcc1 : Wallet;
+  let ethAcc2 : Wallet;
 
   let ethereumContract : Contract;
   let polygonContract : Contract;
-  describe("Simple smokes", () => {
-    before(async () => {
+
+  before(async () => {
     // chain setup
-      ethereum = await createNetwork({
-        name: "Ethereum",
-      });
-
-      polygon = await createNetwork({
-        name: "Polygon",
-      });
-
-      // Extract user wallets for both networks
-      [userEth] = ethereum.userWallets;
-      [userPoly] = polygon.userWallets;
-
-      // contract setup
-      ethereumContract = await deployContract(userEth, SenderReceiver, [
-        ethereum.gateway.address,
-        ethereum.gasService.address,
-      ]);
-      polygonContract = await deployContract(userPoly, SenderReceiver, [
-        polygon.gateway.address,
-        polygon.gasService.address,
-      ]);
+    ethereum = await createNetwork({
+      name: "Ethereum",
     });
 
-    after(async () => {
-      await relay();
+    polygon = await createNetwork({
+      name: "Polygon",
     });
 
+    // Extract user wallets for both networks
+    [userEth, ethAcc1, ethAcc2] = ethereum.userWallets;
+    [userPoly] = polygon.userWallets;
+
+    // contract setup
+    ethereumContract = await deployContract(userEth, SenderReceiver, [
+      ethereum.gateway.address,
+      ethereum.gasService.address,
+    ]);
+    polygonContract = await deployContract(userPoly, SenderReceiver, [
+      polygon.gateway.address,
+      polygon.gasService.address,
+    ]);
+  });
+
+  after(async () => {
+    await relay();
+  });
+
+  describe("Simple smokes", () => {
     it("should set correct gateway and gas service addresses on src and dest chains", async () => {
       expect(await ethereumContract.gateway()).to.equal(ethereum.gateway.address);
       expect(await ethereumContract.gasService()).to.equal(ethereum.gasService.address);
@@ -53,14 +60,16 @@ describe("Axelar Tests", () => {
       expect(await polygonContract.gasService()).to.equal(polygon.gasService.address);
     });
 
-    it("should set string `message` in state from Eth to Poly", async () => {
+    it.skip("should set string `message` in state from Eth to Poly", async () => {
       const message = "Hello Polygon from Ethereum";
-      await ethereumContract.sendMessage(
+
+      const tx = await ethereumContract.sendPayload(
         polygon.name,
         polygonContract.address,
         message,
-        { value: 1e18.toString() },
+        { value: 2e18.toString() },
       );
+      await tx.wait();
 
       await relay();
 
@@ -69,7 +78,7 @@ describe("Axelar Tests", () => {
       expect(polyMessage).to.equal(message);
     });
 
-    it("should set uint256 status in state from Eth to Poly", async () => {
+    it.skip("should set uint256 status in state from Eth to Poly", async () => {
       const status = 12345;
       await ethereumContract.sendStatus(
         polygon.name,
@@ -84,18 +93,136 @@ describe("Axelar Tests", () => {
 
       expect(polyStatus.toString()).to.equal(BigNumber.from(status).toString());
     });
+  });
 
-    it("should pay gas via axelar gas service", async () => {
-      const message = "Hello Polygon from Ethereum";
-      const payload = utils.defaultAbiCoder.encode(
-        ["uint256", "string"],
-        [1, message]
+  describe("Direct Gateway Calls", () => {
+    it("should call #sendPayload() successfully for any simple method", async () => {
+      const message = "Hello Ethereum from Polygon";
+      const status = 5;
+      const setStatusPayload = iface.encodeFunctionData(
+        "setStatus",
+        [ 5 ]
       );
-      const hashedPayload = utils.keccak256(payload);
-      await ethereumContract.sendMessage(
+      const setMessagePayload = iface.encodeFunctionData(
+        "setMessage",
+        [ message ]
+      );
+
+      // internal setStatus() call
+      await polygonContract.connect(userPoly).sendPayload(
+        ethereum.name,
+        ethereumContract.address,
+        setStatusPayload,
+        { value: 1e18.toString() }
+      );
+
+      await polygonContract.connect(userPoly).sendPayload(
+        ethereum.name,
+        ethereumContract.address,
+        setMessagePayload,
+        { value: 1e18.toString() }
+      );
+
+      await relay();
+
+      const ethMsg = await ethereumContract.message();
+      const ethStatus = await ethereumContract.status();
+
+      expect(ethMsg).to.equal(message);
+      expect(ethStatus.toString()).to.equal(status.toString());
+
+      // try calling the other way
+      const setPolyStatusPayload = iface.encodeFunctionData(
+        "setStatus",
+        [ 10 ]
+      );
+
+      const setPolyMessagePayload = iface.encodeFunctionData(
+        "setMessage",
+        [ "Hello Polygon from Ethereum" ]
+      );
+
+      // internal setStatus() call
+      await ethereumContract.connect(userEth).sendPayload(
         polygon.name,
         polygonContract.address,
-        message,
+        setPolyStatusPayload,
+        { value: 1e18.toString() }
+      );
+
+      await ethereumContract.connect(userEth).sendPayload(
+        polygon.name,
+        polygonContract.address,
+        setPolyMessagePayload,
+        { value: 1e18.toString() }
+      );
+
+      await relay();
+
+      const polyMsg = await polygonContract.message();
+      const polyStatus = await polygonContract.status();
+
+      expect(polyMsg).to.equal("Hello Polygon from Ethereum");
+      expect(polyStatus.toString()).to.equal("10");
+    });
+
+    it("should set a mapping entry as a struct through #sendPayload()", async () => {
+      // polygon to ethereum
+      const key = "coolKey";
+      const text = "A very cool text!";
+      const bytes = utils.defaultAbiCoder.encode(["string"], [text]);
+
+      const structVal = {
+        addresses: [
+          ethAcc1.address,
+          ethAcc2.address,
+          userEth.address,
+        ],
+        num: 17981,
+        text,
+        hash: utils.keccak256(bytes),
+        data: bytes,
+      };
+
+      const setMappingPayload = iface.encodeFunctionData(
+        "setDataMapping",
+        [ key, structVal ]
+      );
+
+      await polygonContract.connect(userPoly).sendPayload(
+        ethereum.name,
+        ethereumContract.address,
+        setMappingPayload,
+        { value: 1e18.toString() }
+      );
+
+      await relay();
+
+      const mappingVal = await ethereumContract.dataMapping(key);
+      const mappingAddresses = await ethereumContract.getMappingAddresses(key);
+
+      expect(mappingAddresses[0]).to.equal(structVal.addresses[0]);
+      expect(mappingAddresses[1]).to.equal(structVal.addresses[1]);
+      expect(mappingAddresses[2]).to.equal(structVal.addresses[2]);
+      expect(mappingVal.num.toString()).to.equal(structVal.num.toString());
+      expect(mappingVal.text).to.equal(structVal.text);
+      expect(mappingVal.hash).to.equal(structVal.hash);
+      expect(mappingVal.data).to.equal(structVal.data);
+    });
+
+    it("should pay gas via axelar gas service", async () => {
+      const status = 117425;
+      const payload = iface.encodeFunctionData(
+        "setStatus",
+        [ status ]
+      );
+
+      const hashedPayload = utils.keccak256(payload);
+
+      await ethereumContract.sendPayload(
+        polygon.name,
+        polygonContract.address,
+        payload,
         { value: 1e18.toString() },
       );
 
@@ -109,57 +236,6 @@ describe("Axelar Tests", () => {
       expect(event.args.payloadHash).to.equal(hashedPayload);
       expect(event.args.gasFeeAmount.toString()).to.equal(1e18.toString());
       expect(event.args.refundAddress).to.equal(userEth.address);
-    });
-  });
-
-  describe("Direct Gateway Calls", () => {
-    before(async () => {
-      ethereum = await createNetwork({
-        name: "Ethereum",
-      });
-
-      polygon = await createNetwork({
-        name: "Polygon",
-      });
-
-      // Extract user wallets for both networks
-      [userEth] = ethereum.userWallets;
-      [userPoly] = polygon.userWallets;
-
-      // contract setup
-      ethereumContract = await deployContract(userEth, SenderReceiver, [
-        ethereum.gateway.address,
-        ethereum.gasService.address,
-      ]);
-    });
-
-    after(async () => {
-      await relay();
-    });
-
-    it.only("should call #sendMessage() directly from Gateway", async () => {
-      const message = "Hello Polygon from Ethereum";
-      const iface = new utils.Interface(SenderReceiver.abi);
-      const payload = iface.encodeFunctionData(
-        "setStatus",
-        [ 5 ]
-      );
-
-      const tx = await polygon.gateway.connect(userPoly).callContract(
-        ethereum.name,
-        ethereumContract.address,
-        payload,
-        // { gasLimit: 2e6.toString() }, // TODO: how do we provide gas payment?
-      );
-      await tx.wait();
-
-      await relay();
-
-      // TODO: how do we debug this?
-      const ethMsg = await ethereumContract.message();
-      const ethStatus = await ethereumContract.status();
-
-      expect(ethMsg).to.equal(message);
     });
   });
 });
