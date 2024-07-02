f9d396d4ba8f2a44b16e5d67bea2c04f0ecd834a
Whytecrowe <dor7an@gmail.com>
2024-07-02 14:54:30 -0700

    update mock pragma

--- contracts/upgrade-test-mocks/resolver/ZNSStringResolverMock.sol
+++ contracts/upgrade-test-mocks/resolver/ZNSStringResolverMock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.26;
 
 import { ZNSStringResolver } from "../../resolver/ZNSStringResolver.sol";
 import { UpgradeMock } from "../UpgradeMock.sol";
