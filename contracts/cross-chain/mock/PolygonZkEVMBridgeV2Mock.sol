fb06bcc24f0169638f3f59100df7d287d6016bc8
Whytecrowe <dor7an@gmail.com>
2024-10-08 15:31:08 -0700

    create ZkEvmBridge mock

---
+++ contracts/cross-chain/mock/PolygonZkEVMBridgeV2Mock.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+// This is here only to bring the contract so Hardhat compiles it by default
+import { IBridgeMessageReceiver } from "@zero-tech/zkevm-contracts/contracts/interfaces/IBridgeMessageReceiver.sol";
+import { ERC20Mock } from "../../token/mocks/ERC20Mock.sol";
+
+
+// Note that this does NOT inherit the original Bridge contract!
+// We are EMULATING bridge behavior here so we can run tests on the same chain
+contract PolygonZkEVMBridgeV2Mock {
+    error NoValueInMessagesOnGasTokenNetworks();
+    error DestinationNetworkInvalid();
+    error MessageFailed();
+
+    event ClaimEvent(
+        uint256 globalIndex,
+        uint32 originNetwork,
+        address originAddress,
+        address destinationAddress,
+        uint256 amount
+    );
+    event BridgeEvent(
+        uint8 leafType,
+        uint32 originNetwork,
+        address originAddress,
+        uint32 destinationNetwork,
+        address destinationAddress,
+        uint256 amount,
+        bytes metadata,
+        uint32 globalIndex
+    );
+
+
+    uint256 internal constant _DEPOSIT_CONTRACT_TREE_DEPTH = 32;
+    uint8 private constant _LEAF_TYPE_MESSAGE = 1;
+
+    uint32 public networkID;
+    uint256 public depositCount;
+
+    // TODO multi: in original contract there is TokenWrapped type here
+    // WETH address
+    ERC20Mock public WETHToken;
+
+    constructor(
+        uint32 _networkID,
+        address _WETHToken
+    ) {
+        networkID = _networkID;
+        WETHToken = ERC20Mock(_WETHToken);
+    }
+
+    function bridgeMessage(
+        uint32 destinationNetwork,
+        address destinationAddress,
+        bool forceUpdateGlobalExitRoot,
+        bytes calldata metadata
+    ) external payable {
+        // If exist a gas token, only allow call this function without value
+        if (msg.value != 0 && address(WETHToken) != address(0)) {
+            revert NoValueInMessagesOnGasTokenNetworks();
+        }
+
+        _bridgeMessage(
+            destinationNetwork,
+            destinationAddress,
+            msg.value,
+            forceUpdateGlobalExitRoot,
+            metadata
+        );
+    }
+
+    function _bridgeMessage(
+        uint32 destinationNetwork,
+        address destinationAddress,
+        uint256 amountEther,
+        bool forceUpdateGlobalExitRoot,
+        bytes calldata metadata
+    ) internal {
+        if (destinationNetwork == networkID) {
+            revert DestinationNetworkInvalid();
+        }
+
+        ++depositCount;
+
+        emit BridgeEvent(
+            _LEAF_TYPE_MESSAGE,
+            networkID,
+            msg.sender,
+            destinationNetwork,
+            destinationAddress,
+            amountEther,
+            metadata,
+            uint32(depositCount)
+        );
+    }
+
+    function claimMessage(
+        bytes32[_DEPOSIT_CONTRACT_TREE_DEPTH] calldata smtProofLocalExitRoot,
+        bytes32[_DEPOSIT_CONTRACT_TREE_DEPTH] calldata smtProofRollupExitRoot,
+        uint256 globalIndex,
+        bytes32 mainnetExitRoot,
+        bytes32 rollupExitRoot,
+        uint32 originNetwork,
+        address originAddress,
+        uint32 destinationNetwork,
+        address destinationAddress,
+        uint256 amount,
+        bytes calldata metadata
+    ) external {
+        // Destination network must be this networkID
+        if (destinationNetwork != networkID) {
+            revert DestinationNetworkInvalid();
+        }
+
+        // Execute message
+        bool success;
+        if (address(WETHToken) == address(0)) {
+            // Native token is ether
+            // Transfer ether
+            /* solhint-disable avoid-low-level-calls */
+            (success, ) = destinationAddress.call{value: amount}(
+                abi.encodeCall(
+                    IBridgeMessageReceiver.onMessageReceived,
+                    (originAddress, originNetwork, metadata)
+                )
+            );
+        } else {
+            // Mint wETH tokens
+            WETHToken.mint(destinationAddress, amount);
+
+            // Execute message
+            /* solhint-disable avoid-low-level-calls */
+            (success, ) = destinationAddress.call(
+                abi.encodeCall(
+                    IBridgeMessageReceiver.onMessageReceived,
+                    (originAddress, originNetwork, metadata)
+                )
+            );
+        }
+
+        if (!success) {
+            revert MessageFailed();
+        }
+
+        emit ClaimEvent(
+            globalIndex,
+            originNetwork,
+            originAddress,
+            destinationAddress,
+            amount
+        );
+    }
+}
