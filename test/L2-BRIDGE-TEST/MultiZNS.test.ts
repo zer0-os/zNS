ca59e36f7bddef573e74e059685f9ad3a749a121
Whytecrowe <dor7an@gmail.com>
2024-10-23 14:30:14 -0700

    convert tests to use Campaign deploy

--- test/L2-BRIDGE-TEST/MultiZNS.test.ts
+++ test/L2-BRIDGE-TEST/MultiZNS.test.ts
@@ -14,27 +14,10 @@ import {
 } from "../helpers";
 import { expect } from "chai";
 import * as ethers from "ethers";
-import {
-  PolygonZkEVMBridgeV2Mock,
-  PolygonZkEVMBridgeV2Mock__factory,
-  ZNSChainResolver,
-  ZNSChainResolver__factory, ZNSEthereumPortal,
-  ZNSEthereumPortal__factory, ZNSPolygonZkEvmPortal,
-  ZNSZChainPortal__factory,
-} from "../../typechain";
 import { getDomainHashFromEvent, getEvents } from "../helpers/events";
-import { AddressLike, ContractTransactionReceipt } from "ethers";
-import { PORTAL_ROLE, ResolverTypes } from "../../src/deploy/constants";
-
-
-// TODO multi: move below code to appropriate places
-//  some of these may be optional ??
-export interface IZNSContractsExtended extends IZNSContracts {
-  zPortal : ZNSPolygonZkEvmPortal;
-  bridgeL1 : PolygonZkEVMBridgeV2Mock;
-  ethPortal : ZNSEthereumPortal;
-  bridgeL2 : PolygonZkEVMBridgeV2Mock;
-}
+import { ContractTransactionReceipt } from "ethers";
+import { SupportedChains } from "../../src/deploy/missions/contracts/cross-chain/portals/get-portal-dm";
+import { resetMongoAdapter } from "@zero-tech/zdc";
 
 
 export const NETWORK_ID_L1_DEFAULT = 1n;
@@ -44,102 +27,19 @@ const zChainID = 336699n;
 const zChainName = "ZChain";
 
 
-// TODO multi: add this to the Campaign properly !!!
-export const deployCrossChainContracts = async ({
-  deployer,
-  znsL1,
-  chainResolverAddressL1,
-  znsL2,
-  srcNetworkId = NETWORK_ID_L1_DEFAULT,
-  destNetworkId = NETWORK_ID_L2_DEFAULT,
-  wethTokenAddress = hre.ethers.ZeroAddress,
-  destChainName = zChainName,
-  destChainId = zChainID,
-} : {
-  deployer : SignerWithAddress;
-  znsL1 : IZNSContracts;
-  chainResolverAddressL1 : AddressLike;
-  znsL2 : IZNSContracts;
-  srcNetworkId ?: bigint;
-  destNetworkId ?: bigint;
-  wethTokenAddress ?: string;
-  destChainName ?: string;
-  destChainId ?: bigint;
-}) => {
-  const zPortalFact = new ZNSZChainPortal__factory(deployer);
-  const ethPortalFact = new ZNSEthereumPortal__factory(deployer);
-  const bridgeMockFact = new PolygonZkEVMBridgeV2Mock__factory(deployer);
-
-  const bridgeL1 = await bridgeMockFact.deploy(
-    srcNetworkId,
-    wethTokenAddress,
-  );
-  await bridgeL1.waitForDeployment();
-  const bridgeL2 = await bridgeMockFact.deploy(
-    destNetworkId,
-    wethTokenAddress,
-  );
-  await bridgeL2.waitForDeployment();
-
-  const zPortal = await hre.upgrades.deployProxy(
-    zPortalFact,
-    [
-      destNetworkId,
-      destChainName,
-      destChainId,
-      bridgeL1.target,
-      {
-        accessController: znsL1.accessController.target,
-        rootRegistrar: znsL1.rootRegistrar.target,
-        subRegistrar: znsL1.subRegistrar.target,
-        treasury: znsL1.treasury.target,
-        chainResolver: chainResolverAddressL1,
-        registry: znsL1.registry.target,
-      },
-    ],
-    {
-      kind: "uups",
-    }
-  ) as unknown as ZNSPolygonZkEvmPortal;
-  await zPortal.waitForDeployment();
-
-  const ethPortal = await hre.upgrades.deployProxy(
-    ethPortalFact,
-    [
-      znsL2.accessController.target,
-      bridgeL2.target,
-      zPortal.target,
-      znsL2.registry.target,
-      znsL2.domainToken.target,
-      znsL2.rootRegistrar.target,
-      znsL2.subRegistrar.target,
-    ],
-    {
-      kind: "uups",
-    }
-  ) as unknown as ZNSEthereumPortal;
-  await ethPortal.waitForDeployment();
-
-  // link portals
-  await zPortal.connect(deployer).setDestZnsPortal(ethPortal.target);
-
-  // give Role to the Portal on L2 to call special function
-  await znsL2.accessController.connect(deployer).grantRole(
-    PORTAL_ROLE,
-    ethPortal.target
-  );
-
-  return {
-    zPortal,
-    ethPortal,
-    bridgeL1,
-    bridgeL2,
-  };
+const resetEnvVars = () => {
+  delete process.env.SRC_CHAIN_NAME;
+  delete process.env.MOCK_ZKEVM_BRIDGE;
+  delete process.env.NETWORK_ID;
+  delete process.env.DEST_NETWORK_ID;
+  delete process.env.DEST_CHAIN_NAME;
+  delete process.env.DEST_CHAIN_ID;
+  delete process.env.SRC_ZNS_PORTAL;
 };
 
 describe.only("MultiZNS", () => {
-  let znsL1 : IZNSContractsExtended;
-  let znsL2 : IZNSContractsExtended;
+  let znsL1 : IZNSContracts;
+  let znsL2 : IZNSContracts;
 
   let deployAdmin : SignerWithAddress;
   let user : SignerWithAddress;
@@ -164,10 +64,16 @@ describe.only("MultiZNS", () => {
     globalIndex : bigint;
   };
 
-  let chainResolver : ZNSChainResolver;
-
   before(async () => {
     [ deployAdmin, user, subUser ] = await hre.ethers.getSigners();
+
+    // set ENV vars for the Ethereum ZNS deployment
+    process.env.SRC_CHAIN_NAME = SupportedChains.eth;
+    process.env.MOCK_ZKEVM_BRIDGE = "true";
+    process.env.NETWORK_ID = NETWORK_ID_L1_DEFAULT.toString();
+    process.env.DEST_NETWORK_ID = NETWORK_ID_L2_DEFAULT.toString();
+    process.env.DEST_CHAIN_NAME = SupportedChains.z;
+    process.env.DEST_CHAIN_ID = zChainID.toString();
 
     config = await getConfig({
       deployer: deployAdmin,
@@ -178,22 +84,13 @@ describe.only("MultiZNS", () => {
 
     znsL1 = campaignL1.state.contracts;
 
-    // TODO multi: add deploy mission for ChainResolver if decided to leave it!
-    const chainResFact = new ZNSChainResolver__factory(deployAdmin);
-    chainResolver = await hre.upgrades.deployProxy(
-      chainResFact,
-      [
-        znsL1.accessController.target,
-        znsL1.registry.target,
-      ],
-      {
-        kind: "uups",
-      }
-    ) as unknown as ZNSChainResolver;
-    await chainResolver.waitForDeployment();
+    resetMongoAdapter();
 
-    // TODO multi: add constants/enums for Resolver Types and possibly to Registry as well !!!
-    await znsL1.registry.connect(deployAdmin).addResolverType(ResolverTypes.chain, chainResolver.target);
+    // set vars for ZChain ZNS deployment
+    process.env.SRC_CHAIN_NAME = SupportedChains.z;
+    process.env.SRC_ZNS_PORTAL = znsL1.zPortal.target as string;
+    process.env.NETWORK_ID = NETWORK_ID_L2_DEFAULT.toString();
+    process.env.MONGO_DB_NAME = "zns-l2";
 
     config = await getConfig({
       deployer: deployAdmin,
@@ -205,32 +102,11 @@ describe.only("MultiZNS", () => {
 
     znsL2 = campaignL2.state.contracts;
 
+    // set L2 portal address on L1
+    await znsL1.zPortal.connect(deployAdmin).setDestZnsPortal(znsL2.ethPortal.target);
+
     await znsL2.meowToken.mint(deployAdmin.address, 1000000000000000000000n);
     await znsL2.meowToken.connect(deployAdmin).approve(znsL2.treasury.target, ethers.MaxUint256);
-
-    const {
-      zPortal,
-      ethPortal,
-      bridgeL1,
-      bridgeL2,
-    } = await deployCrossChainContracts({
-      deployer: deployAdmin,
-      znsL1,
-      chainResolverAddressL1: chainResolver.target,
-      znsL2,
-    });
-
-    znsL1 = {
-      ...znsL1,
-      zPortal,
-      bridgeL1,
-    };
-
-    znsL2 = {
-      ...znsL2,
-      ethPortal,
-      bridgeL2,
-    };
 
     await znsL1.meowToken.mint(deployAdmin.address, 1000000000000000000000n);
     await znsL1.meowToken.connect(deployAdmin).approve(znsL1.zPortal.target, ethers.MaxUint256);
@@ -238,6 +114,10 @@ describe.only("MultiZNS", () => {
 
     await znsL1.meowToken.mint(user.address, hre.ethers.parseEther("100000000000000"));
     await znsL1.meowToken.connect(user).approve(znsL1.zPortal.target, ethers.MaxUint256);
+  });
+
+  after(async () => {
+    resetEnvVars();
   });
 
   [
@@ -324,7 +204,7 @@ describe.only("MultiZNS", () => {
             expect(event.args.domainOwner).to.equal(user.address);
 
             const bridgeEvents = await getEvents({
-              contract: znsL1.bridgeL1,
+              contract: znsL1.zkEvmBridge,
               eventName: "BridgeEvent",
             });
             ({ args: bridgedEventData } = bridgeEvents[bridgeEvents.length - 1]);
@@ -354,7 +234,7 @@ describe.only("MultiZNS", () => {
               resolver: resolverL1,
             } = await znsL1.registry.getDomainRecord(domainHash);
             expect(ownerL1).to.equal(znsL1.zPortal.target);
-            expect(resolverL1).to.equal(chainResolver.target);
+            expect(resolverL1).to.equal(znsL1.chainResolver.target);
 
             const tokenOwner = await znsL1.domainToken.ownerOf(BigInt(domainHash));
             expect(tokenOwner).to.equal(znsL1.zPortal.target);
@@ -382,7 +262,7 @@ describe.only("MultiZNS", () => {
           });
 
           it("should properly set data in ChainResolver", async () => {
-            const chainData = await chainResolver.resolveChainDataStruct(domainHash);
+            const chainData = await znsL1.chainResolver.resolveChainDataStruct(domainHash);
             expect(chainData.chainId).to.equal(zChainID);
             expect(chainData.chainName).to.equal(zChainName);
             expect(chainData.znsRegistryOnChain).to.equal(hre.ethers.ZeroAddress);
@@ -446,7 +326,7 @@ describe.only("MultiZNS", () => {
             // **NOTE** that we connect as `deployAdmin` here to show that it's not
             // required that this has to be called by the original owner,
             // it can be called by anyone
-            await znsL2.bridgeL2.connect(deployAdmin).claimMessage(
+            await znsL2.zkEvmBridge.connect(deployAdmin).claimMessage(
               dummySmtProof,
               dummySmtProof,
               bridgedEventData.globalIndex,
@@ -461,7 +341,7 @@ describe.only("MultiZNS", () => {
             );
 
             const events = await getEvents({
-              contract: znsL2.bridgeL2,
+              contract: znsL2.zkEvmBridge,
               eventName: "ClaimEvent",
             });
             const event = events[events.length - 1];
