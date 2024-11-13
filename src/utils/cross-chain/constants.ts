cbb97d2f9df4c7c097bb3a711dca447de87c0052
Whytecrowe <dor7an@gmail.com>
2024-11-13 13:04:08 -0800

    constants for the api helpers

---
+++ src/utils/cross-chain/constants.ts
@@ -0,0 +1,10 @@
+
+// ZChain URLs
+export const ZCHAIN_TEST_BRIDGE_API_URL = "https://wilderworld-dev-erigon1-bridge-api.eu-north-2.gateway.fm";
+export const ZCHAIN_TEST_RPC_URL = "https://wilderworld-dev-erigon1-rpc.eu-north-2.gateway.fm/";
+
+export const ZCHAIN_TEST_BRIDGE_ADDRESS = "0xbE57e0450ae99b62997f2F4731bF8D950e06D124";
+
+// Bridge API endpoints
+export const GET_BRIDGE_DEPOSIT_DATA_EP = "/bridges/";
+export const MERKLE_PROOF_EP = "/merkle-proof";
