aab8e680a49c60ff386875b49ab9e832bd54e8e7
Whytecrowe <dor7an@gmail.com>
2024-11-12 16:07:50 -0800

    add helpers to get data from zchain bridge api

---
+++ src/utils/cross-chain/bridge-api.ts
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { GET_BRIDGE_DEPOSIT_DATA_EP, MERKLE_PROOF_EP, ZCHAIN_TEST_BRIDGE_API_URL } from "./constants";
+
+
+export const baseApiGET = async ({
+  apiURL,
+  endpoint,
+  params,
+} : {
+  apiURL : string;
+  endpoint : string;
+  params ?: any;
+}) => {
+  const axio = axios.create({
+    baseURL: apiURL,
+  });
+
+  return axio.get(endpoint, { params });
+};
+
+export const getBridgeDepositData = async ({
+  apiURL = ZCHAIN_TEST_BRIDGE_API_URL,
+  endpoint = GET_BRIDGE_DEPOSIT_DATA_EP,
+  destAddress,
+  params = { limit: 100, offset: 0 },
+} : {
+  apiURL ?: string;
+  endpoint ?: string;
+  destAddress : string;
+  params ?: any;
+}) => {
+  const result = await baseApiGET({
+    apiURL,
+    endpoint: `${endpoint}${destAddress}`,
+    params,
+  });
+
+  const { data: { deposits } } = result;
+
+  return deposits;
+};
+
+export const getMerkeProof = async ({
+  apiURL = ZCHAIN_TEST_BRIDGE_API_URL,
+  endpoint = MERKLE_PROOF_EP,
+  depositCnt,
+  netId,
+} : {
+  apiURL ?: string;
+  endpoint ?: string;
+  depositCnt : number;
+  netId : number;
+}) => {
+  const result = await baseApiGET({
+    apiURL,
+    endpoint,
+    params: { deposit_cnt: depositCnt, net_id: netId },
+  });
+
+  const { data: { proof } } = result;
+
+  return proof;
+};
