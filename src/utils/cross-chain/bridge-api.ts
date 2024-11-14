5b3320869d306ddc506284236a373b22e4207bd3
Whytecrowe <dor7an@gmail.com>
2024-11-13 16:04:56 -0800

    exteneded api scripts to write to local files

--- src/utils/cross-chain/bridge-api.ts
+++ src/utils/cross-chain/bridge-api.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires, camelcase, no-shadow, @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import { GET_BRIDGE_DEPOSIT_DATA_EP, MERKLE_PROOF_EP, ZCHAIN_TEST_BRIDGE_API_URL } from "./constants";
+import path from "path";
+import fs from "fs";
 
 
 export const baseApiGET = async ({
@@ -62,35 +65,49 @@ export const getMerkeProof = async ({
   return proof;
 };
 
+const main = async (args : Array<string>) => {
+  let apiDataReturn : string;
+
+  switch (args[0]) {
+  case "deposit":
+    const destAddress = args[1];
+
+    const deposits = await getBridgeDepositData({ destAddress });
+    console.log("Deposits acquired:", JSON.stringify(deposits, null, "\t"));
+    apiDataReturn = JSON.stringify(deposits[deposits.length - 1], null, "\t");
+    break;
+
+  case "proof":
+    const depositCnt = Number(args[1]);
+    const netId = Number(args[2]);
+
+    let proof = await getMerkeProof({ depositCnt, netId });
+    proof = JSON.stringify(proof, null, "\t");
+    console.log("Proof acquired:", proof);
+    apiDataReturn = proof;
+    break;
+
+  default:
+    throw new Error("Invalid arguments!");
+  }
+
+  if (args[args.length - 1] === "save" && apiDataReturn) {
+    const pathToDir = path.join(process.cwd(), "./test-data");
+    const pathToFile = path.join(pathToDir, `bridge-${args[0]}.json`);
+
+    if (!fs.existsSync(pathToDir)) {
+      fs.mkdirSync(pathToDir);
+    }
+
+    fs.writeFileSync(pathToFile, apiDataReturn);
+  }
+};
+
 const args = process.argv.slice(2);
 
-switch (args[0]) {
-case "deposit":
-  const destAddress = args[1];
-
-  getBridgeDepositData({ destAddress })
-    .then(deposits => {
-      console.log("Deposits acquired:", JSON.stringify(deposits, null, "\t"));
-    })
-    .catch(error => {
-      console.error("Error acquiring deposits!", error);
-    });
-  break;
-
-case "proof":
-  const depositCnt = Number(args[1]);
-  const netId = Number(args[2]);
-
-  getMerkeProof({ depositCnt, netId })
-    .then(proof => {
-      console.log("Proof acquired:", JSON.stringify(proof, null, "\t"));
-    })
-    .catch(error => {
-      console.error("Error acquiring proof!", error);
-    });
-  break;
-
-default:
-  console.error("Invalid arguments!");
-  break;
-}
+main(args)
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
