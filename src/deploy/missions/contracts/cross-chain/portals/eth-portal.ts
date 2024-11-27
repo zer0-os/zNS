78a2262349e790014bfdfb0e157990c1cce45009
Whytecrowe <dor7an@gmail.com>
2024-11-27 15:53:47 -0800

    add cross-chain portal linking post deploy task to ethPortal dm and rework postDeploy on EthereumPortalDM to properly run both tasks

--- src/deploy/missions/contracts/cross-chain/portals/eth-portal.ts
+++ src/deploy/missions/contracts/cross-chain/portals/eth-portal.ts
@@ -1,8 +1,10 @@
-import { BaseDeployMission, TDeployArgs } from "@zero-tech/zdc";
+import { BaseDeployMission, EnvironmentLevels, TDeployArgs } from "@zero-tech/zdc";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { IZNSCampaignConfig, IZNSContracts, IZNSSigner, IZNSZChainCrossConfig } from "../../../../campaign/types";
 import { PORTAL_ROLE, ProxyKinds } from "../../../../constants";
 import { znsNames } from "../../names";
+import { ethers } from "hardhat";
+import { ZNSZChainPortal } from "../../../../../../typechain";
 
 
 export class ZNSEthereumPortalDM extends BaseDeployMission<
@@ -18,6 +20,9 @@ IZNSContracts
 
   contractName = znsNames.ethPortal.contract;
   instanceName = znsNames.ethPortal.instance;
+  // Post Deploy Task Assessement
+  pdNeed1 ?: boolean;
+  pdNeed2 ?: boolean;
 
   async deployArgs () : Promise<TDeployArgs> {
     const {
@@ -52,22 +57,23 @@ IZNSContracts
   }
 
   async needsPostDeploy () {
-    const {
-      accessController,
-      ethPortal,
-    } = this.campaign;
+    this.pdNeed1 = await this.needsPortalRole();
+    this.pdNeed2 = await this.needsLinkToZChainPortal();
 
-    const hasPortalRole = await accessController.isPortal(await ethPortal.getAddress());
-
-    const needs = !hasPortalRole;
-    const msg = needs ? "needs" : "doesn't need";
-
-    this.logger.debug(`${this.contractName} ${msg} post deploy sequence`);
-
-    return needs;
+    return this.pdNeed1 || this.pdNeed2;
   }
 
   async postDeploy () {
+    if (this.pdNeed1) {
+      await this.assignPortalRole();
+    }
+
+    if (this.pdNeed2) {
+      await this.linkToZChainPortal();
+    }
+  }
+
+  async assignPortalRole () {
     const {
       accessController,
       ethPortal,
@@ -80,6 +86,80 @@ IZNSContracts
     );
     await this.awaitConfirmation(tx);
 
-    this.logger.debug(`${this.contractName} post deploy sequence completed`);
+    this.logger.debug(`${this.contractName} has been assigned PORTAL_ROLE`);
+  }
+
+  async needsPortalRole () {
+    const {
+      accessController,
+      ethPortal,
+    } = this.campaign;
+
+    const hasPortalRole = await accessController.isPortal(await ethPortal.getAddress());
+
+    const needs = !hasPortalRole;
+    const msg = needs ? "needs" : "doesn't need";
+
+    this.logger.debug(`${this.contractName} ${msg} to be assigned PORTAL_ROLE`);
+
+    return needs;
+  }
+
+  async needsLinkToZChainPortal () {
+    const {
+      ethPortal,
+      config: {
+        env,
+        deployAdmin,
+        crosschain,
+      },
+    } = this.campaign;
+
+    const {
+      srcZnsPortal,
+      ethAdmin,
+    } = crosschain as IZNSZChainCrossConfig;
+
+    const ethSigner = env === EnvironmentLevels.dev ? deployAdmin : ethAdmin;
+
+    const zChainPortalEth = await ethers.getContractAt(
+      znsNames.zPortal.contract,
+      srcZnsPortal,
+      ethSigner
+    ) as unknown as ZNSZChainPortal;
+
+    const destZnsPortalAddress = await zChainPortalEth.destZnsPortal();
+
+    const needs = destZnsPortalAddress !== await ethPortal.getAddress();
+    const msg = needs ? "needs" : "doesn't need";
+
+    this.logger.debug(`${this.contractName} ${msg} linking to ${znsNames.zPortal.contract} on Ethereum`);
+
+    if (needs) {
+      this.campaign.updateStateContract(znsNames.zPortal.instance, znsNames.zPortal.contract, zChainPortalEth);
+    }
+
+    return needs;
+  }
+
+  async linkToZChainPortal () {
+    const {
+      ethPortal,
+      zChainPortal,
+      config: {
+        crosschain,
+      },
+    } = this.campaign;
+
+    const {
+      srcZnsPortal,
+    } = crosschain as IZNSZChainCrossConfig;
+
+    this.logger.info(`Linking ${this.contractName} to ${znsNames.zPortal.contract} on Ethereum at ${srcZnsPortal}...`);
+
+    const tx = await zChainPortal.setDestZnsPortal(await ethPortal.getAddress());
+    await this.awaitConfirmation(tx);
+
+    this.logger.info(`${this.contractName} linked to ${znsNames.zPortal.contract} successfully.`);
   }
 }
