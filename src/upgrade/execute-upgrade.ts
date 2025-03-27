e726ec9a62c8cc493a38a699a5a6b7e385a77b87
Whytecrowe <dor7an@gmail.com>
2025-03-26 17:58:38 -0700

    create top level execute-upgrade script

---
+++ src/upgrade/execute-upgrade.ts
@@ -0,0 +1,35 @@
+import { getLogger } from "../deploy/logger/create-logger";
+import { getContractDataForUpgrade, upgradeZNS } from "./upgrade";
+import { getMongoAdapter } from "../deploy/db/mongo-adapter/get-adapter";
+
+
+const execute = async () => {
+  const logger = getLogger();
+  const dbAdapter = await getMongoAdapter(logger);
+
+  logger.info("Prepairing contract data for the upgrade...");
+
+  const contractData = await getContractDataForUpgrade(dbAdapter);
+
+  logger.info("Contract data prepared. Starting the upgrade...");
+
+  const znsUpgraded = await upgradeZNS({
+    contractData,
+    dbAdapter,
+    logger,
+  });
+
+  return znsUpgraded;
+};
+
+execute()
+  .then(znsUpgraded => {
+    const log = getLogger();
+    log.info(`Upgraded ${Object.keys(znsUpgraded).length} ZNS contracts.`);
+    process.exit(0);
+  })
+  .catch(e => {
+    const log = getLogger();
+    log.error("Error during upgrade:", e);
+    process.exit(1);
+  });
