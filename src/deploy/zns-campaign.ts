4f2bb39ab04e26aaf2cab93e5c91200e63890a59
Whytecrowe <dor7an@gmail.com>
2025-03-28 13:02:23 -0700

    add multi db update function

--- src/deploy/zns-campaign.ts
+++ src/deploy/zns-campaign.ts
@@ -14,7 +14,7 @@ import {
 import { getMongoAdapter } from "./db/mongo-adapter/get-adapter";
 import { getLogger } from "./logger/create-logger";
 
-// TODO how do we mock certain things for tests
+
 export const runZnsCampaign = async ({
   config,
   provider,
--- src/upgrade/db.ts
+++ src/upgrade/db.ts
@@ -1,6 +1,28 @@
 import * as hre from "hardhat";
 import { MongoDBAdapter } from "../deploy/db/mongo-adapter/mongo-adapter";
+import { IContractData } from "./types";
+import { IDBVersion } from "../deploy/db/mongo-adapter/types";
 
+
+export const updateAllContractsInDb = async ({
+  contractData,
+  dbAdapter,
+} : {
+  contractData : Array<IContractData>;
+  dbAdapter : MongoDBAdapter;
+}) => {
+  for (const { contractName, address } of contractData) {
+    const implAddress = await hre.upgrades.erc1967.getImplementationAddress(
+      address as string
+    );
+
+    await updateContractInDb({
+      dbAdapter,
+      contractName,
+      implAddress,
+    });
+  }
+};
 
 export const updateContractInDb = async ({
   dbAdapter,
@@ -13,9 +35,12 @@ export const updateContractInDb = async ({
 }) => {
   const { abi, bytecode } = hre.artifacts.readArtifactSync(`${contractName}Pausable`);
 
+  const { dbVersion: curVersion } = await dbAdapter.getLatestVersion() as IDBVersion;
+
   await dbAdapter.contracts.updateOne(
     {
       name: contractName,
+      version: curVersion,
     },
     {
       $set: {
