78b35d93f037b725afd98b9bef4865ec29d7c660
Whytecrowe <dor7an@gmail.com>
2025-04-01 14:30:01 -0700

    make dbAdapter only drop when network is hardhat

--- test/mocha-global.ts
+++ test/mocha-global.ts
@@ -1,3 +1,4 @@
+import * as hre from "hardhat";
 import { getMongoAdapter } from "../src/deploy/db/mongo-adapter/get-adapter";
 
 
@@ -7,7 +8,9 @@ export const mochaGlobalSetup = async () => {
 
 export const mochaGlobalTeardown = async () => {
   const mongoAdapter = await getMongoAdapter();
-  // the next line can be commented out to leave the DB after test to manually test
-  await mongoAdapter.dropDB();
+  if (hre.network.name === "hardhat") {
+    // the next line can be commented out to leave the DB after test to manually test
+    await mongoAdapter.dropDB();
+  }
   await mongoAdapter.close();
 };
