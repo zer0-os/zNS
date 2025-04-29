3fdbc0d90edb455de203d0f145623b334a916b26
MichaelKorchagin <benderrodriguez932@Gmail.com>
2025-04-28 21:49:15 -0700

    Corrected overrides. Added more params, general #register fucntion and `totalSupply`.

--- contracts/token/ZNSSubdomainToken.sol
+++ contracts/token/ZNSSubdomainToken.sol
@@ -1,63 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import { AAccessControlled } from "../access/AAccessControlled.sol";
 import { ARegistryWired } from "../registry/ARegistryWired.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import { ERC721Votes } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
+contract SubdomainToken is
+    ERC721,
+    ERC721Enumerable,
+    EIP712,
+    ERC721Votes,
+    ARegistryWired {
 
-contract SubdomainToken is AAccessControlled, ARegistryWired, ERC721Enumerable, ERC721Votes {
+    uint256 private _totalSupply;
+
     constructor(
-      address registry_,
-      address accessController_
+        string memory name_,
+        string memory symbol_,
+        // TODO: do we pass a version?
+        string memory version_,
+        address registry_,
+        address accessController_
     )
-        ERC721("SubdomainToken", "SUBD")
-        EIP712("SubdomainToken", "1")
+        ERC721(name_, symbol_)
+        EIP712(name_, version_)
     {
-        _setAccessController(_accessController);
         _setRegistry(registry_);
     }
 
-    modifier whenRegisterIsOpen() {
-      // find the param
-    }
-
-    function createSubdomainToken(string calldata subdomain) external whenRegisterIsOpen {
-        // chech if the subdomain is already registered
-        bytes32 subdomainHash = keccak256(abi.encodePacked(subdomain));
-        require(
-          !IZNSRegistry(registry_).getDomainRecord(subdomainHash),
-          "Subdomain already registered"
-        );
-
-        // Who will be the owner of the token?
-        // _mint(toParam, newTokenId);
-        // _mint(this.address, newTokenId);
-        // _mint(msg.sender, newTokenId);
-
-        // _delegate(rootOwner?, or SubOwner?);
-
-        // register using the subdomain registrar
-        // ?
-        // IZNSSubRegistrar(subRegistrar).registerSubdomain(to, subdomain, newTokenId);
-    }
-
-    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal override(ERC721, ERC721Votes)
+    function register(address to, uint256 tokenId, string memory _tokenURI)
+        external
     {
-        super._afterTokenTransfer(from, to, tokenId, batchSize);
+        ++_totalSupply;
+        _safeMint(to, tokenId);
+        // _setTokenURI(tokenId, _tokenURI);
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721Votes) {
-        super._burn(tokenId);
+    // TODO: add access control, when it's ready
+    function totalSupply()
+        public
+        view
+        override(ERC721Enumerable)
+        returns (uint256)
+    {
+        return _totalSupply;
+    }
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Votes)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Votes)
+    {
+        super._increaseBalance(account, value);
     }
 
     function supportsInterface(bytes4 interfaceId)
-        public view override(ERC721, ERC721Enumerable)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function setRegistry(address registry_)
+        external
+        override(ARegistryWired)
+    {
+        _setRegistry(registry_);
     }
 }
