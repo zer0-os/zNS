8da65cdefece476ed60bc0b816e3564e3e7f66bc
MichaelKorchagin <benderrodriguez932@Gmail.com>
2025-06-03 15:06:54 -0700

    Removed old proxy overrides in HHConfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,26 +45,6 @@ const config : HardhatUserConfig = {
         },
       },
     ],
-    overrides: {
-      "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol": {
-        version: "0.8.22",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20000,
-          },
-        },
-      },
-      "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol": {
-        version: "0.8.22",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20000,
-          },
-        },
-      },
-    },
   },
   paths: {
     sources: "./contracts",
