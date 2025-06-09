8901ae875b1fd68411d0a4cb5a4cd511546b91ba
Whytecrowe <dor7an@gmail.com>
2025-06-09 15:56:57 -0700

    update natspec and small optimizations for registrars

--- contracts/registrar/ZNSSubRegistrar.sol
+++ contracts/registrar/ZNSSubRegistrar.sol
@@ -17,8 +17,9 @@ import { ARegistrationPause } from "./ARegistrationPause.sol";
 
 /**
  * @title ZNSSubRegistrar.sol - The contract for registering and revoking subdomains of zNS.
+ *
  * @dev This contract has the entry point for registering subdomains, but calls
- * the ZNSRootRegistrar back to finalize registration. Common logic for domains
+ * the `ZNSRootRegistrar` back to finalize registration canonically. Common logic for domains
  * of any level is in the `ZNSRootRegistrar.coreRegister()`.
 */
 contract ZNSSubRegistrar is
@@ -30,13 +31,14 @@ contract ZNSSubRegistrar is
     using StringUtils for string;
 
     /**
-     * @notice State var for the ZNSRootRegistrar contract that finalizes registration of subdomains.
+     * @notice State var for the `ZNSRootRegistrar` contract that finalizes registration of subdomains.
     */
     IZNSRootRegistrar public override rootRegistrar;
 
     /**
      * @notice Mapping of domainHash to distribution config set by the domain owner/operator.
      * These configs are used to determine how subdomains are distributed for every parent.
+     *
      * @dev Note that the rules outlined in the DistributionConfig are only applied to direct children!
     */
     mapping(bytes32 domainHash => DistributionConfig config) public override distrConfigs;
@@ -49,7 +51,7 @@ contract ZNSSubRegistrar is
     /**
      * @notice Mapping of domainHash to mintlist set by the domain owner/operator.
      * These configs are used to determine who can register subdomains for every parent
-     * in the case where parent's DistributionConfig.AccessType is set to AccessType.MINTLIST.
+     * in the case where parent's `DistributionConfig.AccessType` is set to `AccessType.MINTLIST`.
     */
     mapping(bytes32 domainHash => Mintlist mintStruct) public mintlist;
 
@@ -78,21 +80,24 @@ contract ZNSSubRegistrar is
 
     /**
      * @notice Entry point to register a subdomain under a parent domain specified.
+     *
      * @dev Reads the `DistributionConfig` for the parent domain to determine how to distribute,
      * checks if the sender is allowed to register, check if subdomain is available,
      * acquires the price and other data needed to finalize the registration
      * and calls the `ZNSRootRegistrar.coreRegister()` to finalize.
      * If operator is calling the function, the domain owner is set to the owner of the parent domain,
-     * NOT the operator itself!
+     * NOT the operator or caller address!
      * A non-zero optional `tokenOwner` address can be passed to assign the domain token to another address
      * which would mint the token to that address and let that address use the domain without ownership or the ability
      * to revoke it or manage its data in the system. This can let parent domain owner to mint subdomains
      * in the controlled fashion when the parent domain is LOCKED and give these domains to other users while preventing
      * them from transferring the ownership of the domain token or domain itself to another address or sell their own
      * subdomains.
+     * Owner or operator of the parent domain circumvent the price, fee and payments and are able to register
+     * subdomains for free, but owner will be set to the owner of the parent domain.
+     *
      * @param args SubdomainRegisterArgs type struct with props:
      * - `parentHash` The hash of the parent domain to register the subdomain under
-     * - `label` The label of the subdomain to register (e.g. in 0://zero.child the label would be "child").
      * - `domainAddress` (optional) The address to which the subdomain will be resolved to
      * - `tokenOwner` (optional) The address the token will be assigned to, to offer domain usage without ownership
      * - `tokenURI` (required) The tokenURI for the subdomain to be registered
@@ -100,6 +105,7 @@ contract ZNSSubRegistrar is
      * - `paymentConfig` (optional) Payment config for the domain to set on ZNSTreasury in the same tx
      *  > `paymentConfig` has to be fully filled or all zeros. It is optional as a whole,
      *  but all the parameters inside are required.
+     * - `label` The label of the subdomain to register (e.g. in 0://zero.child the label would be "child").
      */
     function registerSubdomain(
         SubdomainRegisterArgs memory args
@@ -175,11 +181,12 @@ contract ZNSSubRegistrar is
      * @notice Allows registering multiple subdomains in a single transaction.
      * This function iterates through an array of `SubdomainRegistrationArgs` objects and registers each subdomain
      * by calling the `registerSubdomain` function for each entry.
+     *
      * @dev This function reduces the number of transactions required to register multiple subdomains,
      * saving gas and improving efficiency. Each subdomain registration is processed sequentially.
      *
-     * ! IMPORTANT: If a subdomain in the `subRegistrations` array has `parentHash = 0x000...` (null hash),
-     * it will be treated as a nested domain.
+     * ! IMPORTANT: If a subdomain in the `subRegistrations` array has `parentHash = 0x000...` (zero/null hash),
+     * it will be treated as a nested domain under the previously registered domain in the argument array.
      * In this case, the parent of the subdomain will be set to the domain hash of the
      * previously registered subdomain in the array. This allows creating multi-level nested domains in a single
      * transaction. For example:
@@ -196,6 +203,7 @@ contract ZNSSubRegistrar is
      *      + `tokenURI`: The URI to assign to the subdomain token.
      *      + `distrConfig`: The distribution configuration for the subdomain.
      *      + `paymentConfig`: The payment configuration for the subdomain.
+     *
      * @return domainHashes An array of `bytes32` hashes representing the registered subdomains.
      */
     function registerSubdomainBulk(
@@ -237,10 +245,12 @@ contract ZNSSubRegistrar is
 
     /**
      * @notice Setter for `distrConfigs[domainHash]`.
-     * Only domain owner/operator or ZNSRootRegistrar can call this function.
-     * @dev This config can be changed by the domain owner/operator at any time or be set
+     * Only domain owner/operator or `ZNSRootRegistrar` can call this function.
+     *
+     * @dev This config can be changed by the domain hash owner/operator at any time or be set
      * after registration if the config was not provided during the registration.
      * Fires `DistributionConfigSet` event.
+     *
      * @param domainHash The domain hash to set the distribution config for
      * @param config The new distribution config to set (for config fields see `IDistributionConfig.sol`)
     */
@@ -266,10 +276,11 @@ contract ZNSSubRegistrar is
     }
 
     /**
-     * @notice One of the individual setters for `distrConfigs[domainHash]`. Sets `pricerContract` field of the struct.
-     * Made to be able to set the pricer contract for a domain without setting the whole config.
+     * @notice One of the individual setters for `distrConfigs[domainHash]`. Sets `pricerContract` and `priceConfig`
+     * fields of the struct.
      * Only domain owner/operator can call this function.
      * Fires `PricerContractSet` event.
+     *
      * @param domainHash The domain hash to set the pricer contract for
      * @param config The price config data for the given pricer
      * @param pricerContract The new pricer contract to set
@@ -298,6 +309,7 @@ contract ZNSSubRegistrar is
      * Made to be able to set the payment type for a domain without setting the whole config.
      * Only domain owner/operator can call this function.
      * Fires `PaymentTypeSet` event.
+     *
      * @param domainHash The domain hash to set the payment type for
      * @param paymentType The new payment type to set
     */
@@ -318,6 +330,7 @@ contract ZNSSubRegistrar is
      * Made to be able to set the access type for a domain without setting the whole config.
      * Only domain owner/operator or ZNSRootRegistrar can call this function.
      * Fires `AccessTypeSet` event.
+     *
      * @param domainHash The domain hash to set the access type for
      * @param accessType The new access type to set
     */
@@ -335,6 +348,7 @@ contract ZNSSubRegistrar is
      * wants to limit subdomain registration to a specific set of addresses.
      * Can be used to add/remove multiple candidates at once. Can only be called by the domain owner/operator.
      * Fires `MintlistUpdated` event.
+     *
      * @param domainHash The domain hash to set the mintlist for
      * @param candidates The array of candidates to add/remove
      * @param allowed The array of booleans indicating whether to add or remove the candidate
@@ -357,6 +371,14 @@ contract ZNSSubRegistrar is
         emit MintlistUpdated(domainHash, ownerIndex, candidates, allowed);
     }
 
+    /**
+     * @notice Checks if a candidate is mintlisted for a given domain.
+     *
+     * @param domainHash The domain hash to check the mintlist for
+     * @param candidate The address to check if it is mintlisted
+     *
+     * @return bool indicating whether the candidate is mintlisted for the domain
+     */
     function isMintlistedForDomain(
         bytes32 domainHash,
         address candidate
@@ -370,6 +392,7 @@ contract ZNSSubRegistrar is
      * Can only be called by the owner/operator of the domain or by `ZNSRootRegistrar` as a part of the
      * `revokeDomain()` flow.
      * Emits `MintlistCleared` event.
+     *
      * @param domainHash The domain hash to clear the mintlist for
      */
     function clearMintlistForDomain(bytes32 domainHash)
@@ -381,6 +404,15 @@ contract ZNSSubRegistrar is
         emit MintlistCleared(domainHash);
     }
 
+    /**
+     * @notice Function to clear the mintlist and set the domain to `AccessType.LOCKED`.
+     * Can only be called by the owner/operator of the domain or by `ZNSRootRegistrar` as a part of the
+     * `revokeDomain()` flow.
+     * This function is used to lock the domain and prevent any further registrations under it.
+     * Emits `MintlistCleared` and `AccessTypeSet` events.
+     *
+     * @param domainHash The domain hash to clear the mintlist and lock
+     */
     function clearMintlistAndLock(bytes32 domainHash)
     external
     override
@@ -391,6 +423,7 @@ contract ZNSSubRegistrar is
 
     /**
      * @notice Sets the registry address in state.
+     *
      * @dev This function is required for all contracts inheriting `ARegistryWired`.
     */
     function setRegistry(address registry_) public override(ARegistryWired, IZNSSubRegistrar) onlyAdmin {
@@ -400,6 +433,7 @@ contract ZNSSubRegistrar is
     /**
      * @notice Setter for `rootRegistrar`. Only admin can call this function.
      * Fires `RootRegistrarSet` event.
+     *
      * @param registrar_ The new address of the ZNSRootRegistrar contract
     */
     function setRootRegistrar(address registrar_) public override onlyAdmin {
@@ -413,6 +447,7 @@ contract ZNSSubRegistrar is
      * @notice Pauses the registration of new domains.
      * Only ADMIN in `ZNSAccessController` can call this function.
      * Fires `RegistrationPauseSet` event.
+     *
      * @dev When registration is paused, only ADMINs can register new domains.
      */
     function pauseRegistration() external override onlyAdmin {
@@ -430,6 +465,7 @@ contract ZNSSubRegistrar is
 
     /**
      * @notice To use UUPS proxy we override this function and revert if `msg.sender` isn't authorized
+     *
      * @param newImplementation The implementation contract to upgrade to
      */
     // solhint-disable-next-line
