3e0b75128b9e664b0a072937d8a2c2488d5748ba
Whytecrowe <dor7an@gmail.com>
2025-06-23 15:53:47 -0700

    Revert "make postDeploy task to pause RootRegistrar if needed"
    This reverts commit b5e9bc056d968168ee1ad7dba893e2bf2637d157.

--- src/deploy/missions/contracts/root-registrar.ts
+++ src/deploy/missions/contracts/root-registrar.ts
@@ -23,9 +23,6 @@ IZNSContracts
   contractName = znsNames.rootRegistrar.contract;
   instanceName = znsNames.rootRegistrar.instance;
 
-  isRegistrar ?: boolean;
-  needsPause ?: boolean;
-
   async deployArgs () : Promise<TDeployArgs> {
     const {
       accessController,
@@ -55,25 +52,18 @@ IZNSContracts
     const {
       accessController,
       rootRegistrar,
-      config: {
-        deployAdmin,
-        pauseRegistration,
-      },
+      config: { deployAdmin },
     } = this.campaign;
 
-    this.isRegistrar = await accessController
+    const isRegistrar = await accessController
       .connect(deployAdmin)
       .isRegistrar(await rootRegistrar.getAddress());
 
-    this.needsPause = pauseRegistration && !await rootRegistrar.registrationPaused();
-
-    const needs = !this.isRegistrar || this.needsPause;
-
-    const msg = needs ? "needs" : "doesn't need";
+    const msg = !isRegistrar ? "needs" : "doesn't need";
 
     this.logger.debug(`${this.contractName} ${msg} post deploy sequence`);
 
-    return needs;
+    return !isRegistrar;
   }
 
   async postDeploy () {
@@ -86,21 +76,11 @@ IZNSContracts
       deployer,
     } = this.campaign;
 
-    if (!this.isRegistrar) {
-      const tx = await accessController
-        .connect(deployAdmin)
-        .grantRole(REGISTRAR_ROLE, await rootRegistrar.getAddress());
+    const tx = await accessController
+      .connect(deployAdmin)
+      .grantRole(REGISTRAR_ROLE, await rootRegistrar.getAddress());
 
-      await deployer.awaitConfirmation(tx);
-    }
-
-    if (this.needsPause) {
-      const tx = await rootRegistrar
-        .connect(deployAdmin)
-        .pauseRegistration();
-
-      await deployer.awaitConfirmation(tx);
-    }
+    await deployer.awaitConfirmation(tx);
 
     this.logger.debug(`${this.contractName} post deploy sequence completed`);
   }
