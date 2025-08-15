0f7b0be3ab93431bb7750467baafe62b0129cd02
James Earle <james@zero.tech>
2025-08-15 15:08:47 -0400

    minor changes for var naming, removing some unnecessary code

--- src/utils/migration/03_airdrop.ts
+++ src/utils/migration/03_airdrop.ts
@@ -13,8 +13,8 @@ const logger = getLogger();
  */
 const main = async () => {
   // Keeping as separate collections from the start will help downstream registration
-  const rootDomainObjects = await getDomains(true);
-  const subdomainObjects = await getDomains(false);
+  const roots = await getDomains(true) as Domain[];
+  const subs = await getDomains(false) as Domain[];
 
   // Track totals owed to users for payments from domain registration
   const userAmounts = new Map<string, Map<string, bigint>>();
@@ -22,16 +22,8 @@ const main = async () => {
   // If payment token resolution fails for a domain we hold onto it for debugging
   const errorDomains = [];
 
-  // Doing this creates strong typing and extensibility that allows
-  // the below `insertMany` calls to add properties to the object for `_id` properly
-  const roots = rootDomainObjects.map(d => d as Domain);
-  logger.info(`Found ${roots.length} root domains`);
-
-  const subs = subdomainObjects.map(d => d as Domain);
-  logger.info(`Found ${subs.length} subdomains`);
-
   // Keep tack of tokens as we iterate for later use
-  const tokenPairs : Map<string, string> = new Map();
+  const tokensMap : Map<string, string> = new Map();
 
   logger.info("Processing...");
   for (const [i,d] of [...roots, ...subs].entries()) {
@@ -55,8 +47,8 @@ const main = async () => {
           // Override the default name if it is specified
           paymentToken = d.parent.treasury.paymentToken.symbol;
 
-          if (!tokenPairs.has(paymentToken)) {
-            tokenPairs.set(paymentToken, d.parent.treasury.paymentToken.id);
+          if (!tokensMap.has(paymentToken)) {
+            tokensMap.set(paymentToken, d.parent.treasury.paymentToken.id);
           }
         } else {
           errorDomains.push(d);
@@ -100,23 +92,17 @@ const main = async () => {
     const user : string = userAmount[0];
     const amountsMap : Map<string, bigint> = userAmount[1];
 
-    // If the user has more payment tokens than there are specified by the TOKEN_PAIRS .env var,
-    // then fail, we cannot calculate their refund and their is likely a mistake somewhere
-    if (amountsMap.size > tokenPairs.size) {
-      throw new Error("Error: User has used more tokens in payment than found in domain iteration");
-    }
-
     // Row to build up as we read the specific token values
     const row = ["erc20"];
 
-    for(const pair of tokenPairs) {
+    for(const token of tokensMap) {
       // 0 is token symbol
-      const amount = amountsMap.get(pair[0]);
+      const amount = amountsMap.get(token[0]);
 
       // Amount may be null for one or both
       if (amount) {
         // 1 is token contract address
-        row.push(pair[1], user, ethers.formatEther(amount).toString());
+        row.push(token[1], user, ethers.formatEther(amount).toString());
       }
     }
 
@@ -129,7 +115,6 @@ const main = async () => {
   }
 
   logger.info(`rows.length: ${rows.length}`);
-
 
   fs.writeFileSync("03_errorDomains.json", JSON.stringify(errorDomains, null, 2));
   fs.writeFileSync("03_userAmounts.csv", `${headers}\n${rows.join("\n")}`);
